c0b721542059e850b0910403e729ccc8099b1fff
Stanislas Ormières <stan@stormier.ninja>
2018-12-21 12:19:59 +0100

    Set up mongoDB

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,16 @@
 import http from 'http'
 
+
 import app from './app'
+import mongoConnect from './mongo-connection'
 
 const PORT = process.env.PORT || 8000
 
-http.createServer(app).listen(PORT, 'localhost')
-
-console.log(`Server running at http://localhost:${PORT}/`)
+mongoConnect()
+  .then(() => {
+    http.createServer(app).listen(PORT, 'localhost')
+    console.log(`Server running at http://localhost:${PORT}/`)
+  })
+  .catch(error => {
+    console.log(`Server could not connect to DB, exiting`)
+  })
---
+++ server/src/mongo-connection.js
@@ -0,0 +1,29 @@
+import delay from 'delay'
+import mongoose from 'mongoose'
+
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/candilib'
+
+let mongoConnectionAttempt = 10
+const delayBeforeAttempt = process.env.NODE_ENV === 'production' ? 2000 : 1000
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(
+      mongoURL,
+      { useNewUrlParser: true },
+    )
+    console.log('Connected to Mongo!')
+    return true
+  } catch (err) {
+    --mongoConnectionAttempt
+    if (mongoConnectionAttempt > 0) {
+      console.warn(`Could not connect to Mongo, ${mongoConnectionAttempt} tries left`)
+      return delay(delayBeforeAttempt).then(connectToMongo)
+    } else {
+      const errorMessage = 'Could not connect to Mongo, make sure it is started and listening on the appropriate port'
+      throw new Error(errorMessage)
+    }
+  }
+}
+
+export default connectToMongo
