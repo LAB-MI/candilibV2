8c6641d9b06a514aecb19620168709cba290bbec
Stanislas Ormières <stan@stormier.ninja>
2018-12-21 17:05:55 +0100

    Refactor mongo connection utility

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,12 @@
 import http from 'http'
 
 import app from './app'
-import mongoConnect from './mongo-connection'
+import { connect } from './mongo-connection'
 import logger from './logger'
 
 const PORT = process.env.PORT || 8000
 
-mongoConnect()
+connect()
   .then(() => {
     http.createServer(app).listen(PORT, 'localhost')
     logger.info(`Server running at http://localhost:${PORT}/`)
