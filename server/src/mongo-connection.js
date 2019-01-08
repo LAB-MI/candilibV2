1b666f8567526c05c9eeef05f943040415ab96f5
Stanislas Ormières <stan@stormier.ninja>
2019-01-08 11:38:12 +0100

    Mock mongoose for the tests

--- server/src/mongo-connection.js
+++ server/src/mongo-connection.js
@@ -1,31 +1,44 @@
 import delay from 'delay'
 import mongoose from 'mongoose'
+import MongoMemoryServer from 'mongodb-memory-server'
 
 import logger from './util/logger'
 
+mongoose.Promise = Promise
+
 const isTest = process.env.NODE_ENV === 'test'
-const dbName = isTest ? 'candilib_test' : 'candilib'
+const dbName = 'candilib'
+
+const mongoServer = new MongoMemoryServer()
 
 const mongoURL = process.env.MONGO_URL || `mongodb://localhost:27017/${dbName}`
 
-let mongoConnectionAttempt = 30
-const delayBeforeAttempt = process.env.NODE_ENV === 'production' ? 2000 : 1000
+let reconnectTries = 30
+const reconnectInterval = process.env.NODE_ENV === 'production' ? 2000 : 1000
+
+const mongooseOpts = {
+  useNewUrlParser: true,
+}
 
 export const connect = async () => {
   try {
+    let mongoUri
+    if (isTest) {
+      mongoUri = await mongoServer.getConnectionString()
+    } else {
+      mongoUri = mongoURL
+    }
     await mongoose.connect(
-      mongoURL,
-      { useNewUrlParser: true }
+      mongoUri,
+      mongooseOpts
     )
     logger.info('Connected to Mongo!')
     return mongoose
   } catch (err) {
-    --mongoConnectionAttempt
-    if (mongoConnectionAttempt > 0) {
-      logger.warn(
-        `Could not connect to Mongo, ${mongoConnectionAttempt} tries left`
-      )
-      return delay(delayBeforeAttempt).then(connect)
+    --reconnectTries
+    if (reconnectTries > 0) {
+      logger.warn(`Could not connect to Mongo, ${reconnectTries} tries left`)
+      return delay(reconnectInterval).then(connect)
     } else {
       const errorMessage =
         'Could not connect to Mongo, make sure it is started and listening on the appropriate port'
@@ -35,6 +48,9 @@ export const connect = async () => {
 }
 
 export const disconnect = async () => {
-  await mongoose.disconnect()
-  logger.info('Disconnected from Mongo')
+  try {
+    await mongoose.disconnect()
+  } catch (error) {
+    logger.info('Disconnected from Mongo')
+  }
 }
