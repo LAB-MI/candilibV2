d42c524623a602b5da13c50754f2a56f42e54f9a
Stanislas Ormières <stan@stormier.ninja>
2019-01-08 16:13:51 +0100

    Add vuetify to the stack

--- back-office/src/main.js
+++ back-office/src/main.js
@@ -1,5 +1,7 @@
 import 'normalize.css'
 import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/dist/vuetify.min.css'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +11,10 @@ import './registerServiceWorker'
 import './main.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify, {
+  iconfont: 'md',
+})
 
 new Vue({
   router,
