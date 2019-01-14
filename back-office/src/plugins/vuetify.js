aadc4af6e844010b2fb8d9fd035243892599f4f0
Stanislas Ormières <stan@stormier.ninja>
2019-01-14 12:36:10 +0100

    Add a drawer

--- back-office/src/plugins/vuetify.js
+++ back-office/src/plugins/vuetify.js
@@ -6,7 +6,7 @@ import fr from 'vuetify/es5/locale/fr'
 import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
-  iconfont: 'fa',
+  iconfont: 'md',
   theme: {
     primary: colors.red.darken1, // #E53935
     secondary: colors.red.lighten4, // #FFCDD2
--- back-office/src/views/admin/Admin.vue
+++ back-office/src/views/admin/Admin.vue
@@ -1,22 +1,66 @@
 <template>
   <div class="admin">
-    <v-toolbar dark fixed>
-      <v-toolbar-title>
-        <h1 class="logo">
-          <router-link to="/admin" class="home-link">C<span class="col-red">A</span>NDILIB</router-link>
-        </h1>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-    </v-toolbar>
-      <header class="admin-header">
-      <button class="menu-burger">
-        <v-icon dark large>menu</v-icon>
-      </button>
-    </header>
-    <p :style="{height: '200vh'}">
-      test
-    </p>
+    <v-container>
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        dark
+        temporary
+      >
+      </v-navigation-drawer>
+      <v-toolbar dark fixed>
+        <v-toolbar-side-icon large @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>
+          <h1 class="logo">
+            <router-link to="/admin" class="home-link">C<span class="col-red">A</span>NDILIB</router-link>
+          </h1>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <div class="text-xs-center d-flex align-center">
+          <v-tooltip bottom>
+            <v-btn icon slot="activator">
+              <v-icon>calendar_today</v-icon>
+            </v-btn>
+            <span>Calendrier</span>
+          </v-tooltip>
+          <v-tooltip bottom>
+            <v-btn icon slot="activator">
+              <v-icon>format_list_numbered</v-icon>
+            </v-btn>
+            <span>Liste des candidats</span>
+          </v-tooltip>
+          <v-tooltip bottom>
+            <v-btn icon slot="activator">
+              <v-icon>import_export</v-icon>
+            </v-btn>
+            <span>Import/Export Aurige</span>
+          </v-tooltip>
+          <v-tooltip bottom>
+            <v-btn icon slot="activator">
+              <v-icon>favorite</v-icon>
+            </v-btn>
+            <span>Liste blanche</span>
+          </v-tooltip>
+        </div>
+      </v-toolbar>
+      <div :style="{margin: '4em 1em 1em 1em'}">
+        <p>
+          test 1
+        </p>
+        <p :style="{height: '200vh'}">
+          test 2
+        </p>
+        <p id="admin-calendar">
+          test 3
+        </p>
+        <p :style="{height: '200vh'}">
+          test 4
+        </p>
+        <p>
+          test 5
+        </p>
+      </div>
+    </v-container>
   </div>
 </template>
 
@@ -24,11 +68,18 @@
 import { CHECK_TOKEN, SIGNED_IN } from '@/store'
 
 export default {
+  data () {
+    return {
+      drawer: false,
+    }
+  },
+
   computed: {
     authStatus () {
       return this.$store.state.auth.status
     },
   },
+
   methods: {
     async checkAuth () {
       await this.$store.dispatch(CHECK_TOKEN)
@@ -37,6 +88,7 @@ export default {
       }
     },
   },
+
   mounted () {
     this.checkAuth()
   },
@@ -48,6 +100,7 @@ export default {
   display: flex;
   padding: 0 1em;
   background-color: black;
+  margin-top: 4em;
 }
 
 .logo {
@@ -63,6 +116,6 @@ export default {
   font-weight: 600;
   text-decoration: none;
   letter-spacing: 0.1rem;
-  font-size: 1.1em;
+  font-size: 1em;
 }
 </style>
