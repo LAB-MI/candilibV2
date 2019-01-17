2189b1f1602d12a0343341089fe5c8a29eec2b9d
baudoin.tran-econocom <baudoin.tran@econocom.com>
2019-01-17 17:24:32 +0100

    export resa

--- server/src/routes/admin/business/export-candidats.js
+++ server/src/routes/admin/business/export-candidats.js
@@ -1,5 +1,5 @@
 import { Parser as Json2csvParser } from 'json2csv'
-
+import moment from 'moment'
 import { findAllCandidatsLean } from '../../../models/candidat'
 
 export const getCandidatsAsCsv = async () => {
@@ -25,6 +25,44 @@ export const getCandidatsAsCsv = async () => {
     {
       label: 'email',
       value: 'email',
+    },
+  ]
+
+  const json2csvParser = new Json2csvParser({ fields })
+  const csv = json2csvParser.parse(candidats)
+
+  return csv
+}
+
+export const getBookedCandidatsAsCsv = async candidats => {
+  const fields = [
+    {
+      label: 'inspecteur',
+      value: 'place.inspecteur',
+    },
+    {
+      label: 'centre',
+      value: 'place.centre',
+    },
+    {
+      label: 'Date réservé',
+      value: (row, field) =>
+        row.place &&
+        row.place.date &&
+        moment(row.place.date).format('YYYY-MM-DD HH:mm'),
+      stringify: true,
+    },
+    {
+      label: 'Code NEPH',
+      value: 'codeNeph',
+    },
+    {
+      label: 'Nom de naissance',
+      value: 'nomNaissance',
+    },
+    {
+      label: 'Prénom',
+      value: 'prenom',
     },
   ]
 
