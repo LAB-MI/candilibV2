bde7a058f7b3aeb3f9181f75f7037abe52e108af
Stanislas Ormières <stan@stormier.ninja>
2019-01-21 17:46:05 +0100

    Extract routes to prepare for conditional builds

--- client/src/router.js
+++ client/src/router.js
@@ -6,41 +6,43 @@ import Error404 from '@/views/Error404.vue'
 
 Vue.use(Router)
 
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: Home,
+    meta: {
+      guest: true,
+    },
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: () => import('./views/admin'),
+    children: [
+      {
+        path: ':tool',
+        name: 'adminTool',
+      },
+    ],
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: '/*',
+    name: '404',
+    component: Error404,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+]
+
 const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'login',
-      component: Home,
-      meta: {
-        guest: true,
-      },
-    },
-    {
-      path: '/admin',
-      name: 'admin',
-      component: () => import('./views/admin'),
-      children: [
-        {
-          path: ':tool',
-          name: 'adminTool',
-        },
-      ],
-      meta: {
-        requiresAuth: true,
-      },
-    },
-    {
-      path: '/*',
-      name: '404',
-      component: Error404,
-      meta: {
-        requiresAuth: true,
-      },
-    },
-  ],
+  routes,
 })
 
 export default router
