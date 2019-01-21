0222f7a9fb9455b11884917a5d17d3fa337e1a58
Stanislas Ormières <stan@stormier.ninja>
2019-01-21 17:32:56 +0100

    Start implementing a calendar view

--- client/src/views/Error404.vue
+++ client/src/views/Error404.vue
@@ -3,3 +3,30 @@
     Page introuvable
   </div>
 </template>
+
+<script>
+import { DateTime } from 'luxon'
+
+import SimpleCalendar from './SimpleCalendar'
+
+export default {
+  components: {
+    SimpleCalendar,
+  },
+
+  data () {
+    return {
+      places: [
+        {
+          _id: 1,
+          start: DateTime.local().set({ hour: 9, minute: 0 }),
+          duration: [30, 'minutes'],
+        },
+      ],
+    }
+  },
+
+  mounted () {
+  },
+}
+</script>
