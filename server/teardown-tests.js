d539c2f2e3e8142c8dba14150cf3993c405c4d9d
Stanislas Ormières <stan@stormier.ninja>
2019-02-08 16:36:16 +0100

    Fix test --watch

--- server/teardown-tests.js
+++
@@ -1,6 +0,0 @@
-const { stopMongoMemoryServer } = require('./src/mongo-memory-server-setup')
-
-module.exports = async () => {
-  console.log('All tests are done, stopping MongoMemoryServer')
-  await stopMongoMemoryServer()
-}
