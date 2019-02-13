ce8c21e74ed9307915edb54937a1d55cbf2b913b
Stanislas Ormières <stan@stormier.ninja>
2019-02-13 12:01:00 +0100

    Set a readable color for error

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -3,14 +3,10 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import fr from 'vuetify/es5/locale/fr'
 
-import colors from 'vuetify/es5/util/colors'
-
 Vue.use(Vuetify, {
   iconfont: 'md',
   theme: {
-    primary: colors.blue.darken1, // #E53935
-    secondary: colors.blue.lighten4, // #FFCDD2
-    accent: colors.indigo.base, // #3F51B5
+    error: '#891329',
   },
   lang: {
     locales: { fr },
