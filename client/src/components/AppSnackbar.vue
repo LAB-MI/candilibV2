7f72b60fc3de43c52c868d1697662fab8c799d4f
Stanislas Ormière <stan@stormier.ninja>
2019-02-20 19:27:49 +0100

    Make a component independant of the store

---
+++ client/src/components/AppSnackbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <pure-app-snackbar :message="message" />
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import { HIDE_MESSAGE } from '@/store'
+
+export default {
+  computed: {
+    ...mapState([
+      'message',
+    ]),
+  },
+
+  methods: {
+    onClose () {
+      this.$store.dispatch(HIDE_MESSAGE)
+    },
+  },
+}
+</script>
---
+++ client/src/components/PureAppSnackbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-snackbar
+    :value="message.show"
+    :color="message.color"
+    bottom
+    :timeout="message.timeout"
+    @input="onClose"
+  >
+    {{ message.content }}
+    <v-btn icon
+      flat
+      @click="onClose"
+    >
+      <v-icon color="white">
+        close
+      </v-icon>
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  props: {
+    message: Object,
+  },
+}
+</script>
