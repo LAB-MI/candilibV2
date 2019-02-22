48ee7701f843d1bddacf840bc1805bc3b7cb75af
jeremy <jclaudan@student.42.fr>
2019-02-22 16:12:19 +0100

    move uploadCandidat implementation to vuex store

--- client/src/store/aurige.js
+++ client/src/store/aurige.js
@@ -1,4 +1,9 @@
+import api from '@/api'
+
+import { SHOW_SUCCESS, SHOW_ERROR } from '@/store'
+
 export const SHOW_AURIGE_RESULT = 'SHOW_AURIGE_RESULT'
+export const AURIGE_UPLOAD_CANDIDATS = 'AURIGE_UPLOAD_CANDIDATS'
 
 export default {
   state: {
@@ -13,6 +18,20 @@ export default {
     async [SHOW_AURIGE_RESULT] ({ commit }, result) {
       const { candidats } = result
       commit(SHOW_AURIGE_RESULT, candidats)
+    },
+    async [AURIGE_UPLOAD_CANDIDATS] ({ commit, dispatch, state }, file) {
+      const data = new FormData()
+      data.append('file', file)
+      try {
+        const result = await api.admin.uploadCandidatsJson(data)
+        if (result.success === false) {
+          throw new Error(result.message)
+        }
+        commit(SHOW_AURIGE_RESULT, result)
+        dispatch(SHOW_SUCCESS, result.message)
+      } catch (error) {
+        dispatch(SHOW_ERROR, error.message)
+      }
     },
   },
 }
--- client/src/views/admin/components/Aurige.vue
+++ client/src/views/admin/components/Aurige.vue
@@ -26,7 +26,7 @@
 <script>
 import api from '@/api'
 import { downloadContent } from '@/util'
-import { SHOW_INFO, SHOW_SUCCESS, SHOW_ERROR, SHOW_AURIGE_RESULT } from '@/store'
+import { SHOW_INFO, SHOW_SUCCESS, SHOW_ERROR, SHOW_AURIGE_RESULT, AURIGE_UPLOAD_CANDIDATS } from '@/store'
 import AurigeValidation from './AurigeValidation'
 import UploadFile from '@/components/UploadFile.vue'
 
@@ -44,7 +44,6 @@ export default {
   data () {
     return {
       file: undefined,
-      lastFile: undefined,
     }
   },
 
@@ -60,24 +59,9 @@ export default {
       const message = `Fichier ${file.name} prêt à être synchronisé`
       this.$store.dispatch(SHOW_INFO, message, 1000)
     },
-
-    async uploadCandidats (data) {
-      this.lastFile = this.file
-      try {
-        this.file = null
-        const result = await api.admin.uploadCandidatsJson(data)
-        if (result.success === false) {
-          throw new Error(result.message)
-        }
-        this.$store.dispatch(SHOW_AURIGE_RESULT, result)
-        this.$store.dispatch(SHOW_SUCCESS, result.message)
-        this.lastFile = null
-      } catch (error) {
-        this.file = this.lastFile
-        this.$store.dispatch(SHOW_ERROR, error.message)
-      }
+    async uploadCandidats () {
+        this.$store.dispatch(AURIGE_UPLOAD_CANDIDATS, this.file)
     },
-
     async getCandidatsAsCsv () {
       const response = await api.admin.exportCsv()
       downloadContent(response)
