95b1b6e98467b4b6cf2a51bbea7976905c1fb8f4
jeremy <jclaudan@student.42.fr>
2019-02-22 19:05:32 +0100

    add faq directory

---
+++ client/src/views/candidat/faq/Faq.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <h2>
+    FAQ
+  </h2>
+  <ul>
+   <li v-for="(item, index) in arrayContent" :key="index">
+    <FaqContent :item="item"/>
+   </li>
+  </ul>
+</div>
+</template>
+
+<script>
+import FaqContent from './FaqContent.vue'
+import { faqJson } from './FaqJson'
+
+export default {
+  name: 'faq',
+  components: {
+    FaqContent,
+  },
+  methods: {
+  },
+  data () {
+    return {
+      arrayContent: faqJson,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/views/candidat/faq/FaqContent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <a>
+      {{ item.title }}
+        <i class="material-icons">
+          {{ 'add_circle' }}
+        </i>
+        <i class="material-icons">
+          {{ 'remove_circle' }}
+        </i>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'faq',
+  props: {
+    item: Object,
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<style>
+
+</style>
