7c8c5fa7dabfac611a0fbc4eb7e4543a3e956402
jclaudan <jclaudan@student.42.fr>
2019-03-01 18:58:40 +0100

    add v-toolbar fixed

--- client/src/views/candidat/components/date-choice/DateChoiceContent.vue
+++ client/src/views/candidat/components/date-choice/DateChoiceContent.vue
@@ -1,32 +1,39 @@
 <template>
-  <v-tabs
-      fixed-tabs
-      centered
-      color="red"
-      dark
-      next-icon="mdi-arrow-right-bold-box-outline"
-      prev-icon="mdi-arrow-left-bold-box-outline"
-      show-arrows
-  >
-    <v-tabs-slider color="black"></v-tabs-slider>
-    <v-tab v-for="(month, index) in monthBlock" :key="index" :href="'#tab-'+ month.month">
-      {{ month.month }}
-    </v-tab>
-      <v-tabs-items>
-    <v-tab-item
-      v-for="i in monthBlock"
-      :key="i"
-      :value="'tab-' + i.month"
-    >
-      <v-card flat>
-        <v-card-text>
-          <date-choice-sub-content :items="i.daysAndHoursDispo">
-          </date-choice-sub-content>
-        </v-card-text>
-      </v-card>
-    </v-tab-item>
-      </v-tabs-items>
-  </v-tabs>
+  <div>
+    <!-- <v-toolbar dark fixed>
+         <v-toolbar-title>Title</v-toolbar-title>
+    </v-toolbar> -->
+      <v-tabs
+          fixed-tabs
+          centered
+          color="red"
+          dark
+          next-icon="mdi-arrow-right-bold-box-outline"
+          prev-icon="mdi-arrow-left-bold-box-outline"
+          show-arrows
+      >
+        <v-tabs-slider color="black"></v-tabs-slider>
+        <v-toolbar dark fixed>
+        <v-tab v-for="(month, index) in monthBlock" :key="index" :href="'#tab-'+ month.month">
+          {{ month.month }}
+        </v-tab>
+        </v-toolbar>
+        <v-tabs-items>
+        <v-tab-item
+          v-for="i in monthBlock"
+          :key="i"
+          :value="'tab-' + i.month"
+        >
+          <v-card flat>
+            <v-card-text>
+              <date-choice-sub-content :items="i.daysAndHoursDispo">
+              </date-choice-sub-content>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+        </v-tabs-items>
+      </v-tabs>
+  </div>
 </template>
 
 <script>
