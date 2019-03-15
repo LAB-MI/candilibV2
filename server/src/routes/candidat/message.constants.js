b673790282014fabdcfe88b4d2ff91d384eac25b
baudoin.tran-econocom <baudoin.tran@econocom.com>
2019-03-15 19:06:22 +0100

    resend mail convocation

--- server/src/routes/candidat/message.constants.js
+++ server/src/routes/candidat/message.constants.js
@@ -8,3 +8,9 @@ export const CANCEL_RESA_WITH_NO_MAIL_SENT =
   'Votre annulation a bien été prise en compte.'
 export const SAME_RESA_ASKED =
   "Votre demande ne peut être prise en compte : Il s'agit de la même réservation que celle que vous avez déjà."
+export const SEND_MAIL_ASKED =
+  'Votre convocation a été envoyée dans votre boîte mail.'
+export const FAILED_SEND_MAIL_ASKED =
+  "Désolé, votre convocation n'a pas pu être envoyée dans votre boîte mail. Veuillez réessayer plus tard."
+export const SEND_MAIL_ASKED_RESA_EMPTY =
+  "Vous n'avez pas de réservation. Aucun convocation n'a été envoyé dans votre boîte mail."
