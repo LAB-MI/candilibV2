ce55f16011df4b22c35780a1b382edf3b4826a09
jclaudan <jclaudan@student.42.fr>
2019-03-20 11:26:26 +0100

    fix issue on centerSelection #160

--- client/src/views/CandidatHome.vue
+++ client/src/views/CandidatHome.vue
@@ -21,6 +21,7 @@ import { mapState } from 'vuex'
 
 import {
   FETCH_CANDIDAT_RESERVATION_REQUEST,
+  FETCH_MY_PROFILE_REQUEST,
   SHOW_ERROR,
 } from '@/store'
 
@@ -52,11 +53,20 @@ export default {
         this.$store.dispatch(SHOW_ERROR, error.message)
       }
     },
+
+    async getMyProfile () {
+      try {
+        await this.$store.dispatch(FETCH_MY_PROFILE_REQUEST)
+      } catch (error) {
+        this.$store.dispatch(SHOW_ERROR, error.message)
+      }
+    },
   },
 
   async mounted () {
     this.$router.replace({ name: 'candidat-presignup' })
-    this.getCandidatReservation()
+    await this.getCandidatReservation()
+    await this.getMyProfile()
   },
 }
 </script>
