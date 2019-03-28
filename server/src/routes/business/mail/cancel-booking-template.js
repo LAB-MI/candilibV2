4819a0cc0e47925fcb54eedde97b335bca364a39
baudoin.tran-econocom <baudoin.tran@econocom.com>
2019-03-28 13:39:30 +0100

    add reservation in mail cancellation template

--- server/src/routes/business/mail/cancel-booking-template.js
+++ server/src/routes/business/mail/cancel-booking-template.js
@@ -1,9 +1,16 @@
 export const getCancelBookingTemplate = (
   nomNaissance,
-  codeNeph
+  codeNeph,
+  nameCentre,
+  dateResa,
+  houreResa
 ) => `<p>Madame, Monsieur ${nomNaissance},</p>
 <br>
-<p>votre réservation à l'examen pratique du permis de conduire avec
-le numéro NEPH ${codeNeph} est bien annulée. </p>
+<p>
+  Votre réservation à l'examen pratique du permis de conduire 
+  à ${nameCentre} le ${dateResa} à ${houreResa} avec le numéro NEPH ${codeNeph} est bien annulée.
+  Vous pouvez choisir un autre créneau disponible.
+</p>
+
 <br>
 <p align="right">L'équipe Candilib</p>`
