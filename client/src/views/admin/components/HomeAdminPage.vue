519289e56c738dbcca32ba9f6906bc565e7b9855
jclaudan <jclaudan@student.42.fr>
2019-04-11 10:48:15 +0200

    Remove content of home page #138

--- client/src/views/admin/components/HomeAdminPage.vue
+++ client/src/views/admin/components/HomeAdminPage.vue
@@ -3,22 +3,5 @@
     <h1>
       Home page admin
     </h1>
-    <label >Define departement</label> <p />
-    <input placeholder="Define departement"/> <p />
-    <label >Search candidat</label> <p />
-    <input placeholder="search candidat"/> <p />
-    <label >search instpecteur</label> <p />
-    <input placeholder="search instpecteur" />
-    <div>
-      <h1>
-        alert crenaux annule
-      </h1>
-      <h1>
-        list valide par aurige
-      </h1>
-      <h1>
-        list resulta de la veille
-      </h1>
-    </div>
   </div>
 </template>
