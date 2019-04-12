212c59e3a1083cc35a99d17fe90c8fb26388b88a
jclaudan <jclaudan@student.42.fr>
2019-04-12 16:01:13 +0200

    WIP add plugin vue-carousel and weekmonitor component

--- client/src/views/admin/components/HomeAdminPage.vue
+++ client/src/views/admin/components/HomeAdminPage.vue
@@ -3,5 +3,28 @@
     <h1>
       Home page admin
     </h1>
+    <v-container fluid>
+      <v-layout row wrap>
+        <v-flex class="monitor-wrapper" v-for="i in 4" :key="`6${i}`" xs6>
+          <week-monitor :nameCenter="'nameCenter' + i" />
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
+
+<script>
+import WeekMonitor from './WeekMonitor.vue'
+
+export default {
+  components: {
+    WeekMonitor,
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.monitor-wrapper {
+  padding: 2em;
+}
+</style>
---
+++ client/src/views/admin/components/WeekMonitor.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h2>{{ nameCenter }}</h2>
+    <carousel
+      class="carousel"
+      :navigationEnabled="true"
+      :paginationEnabled="false"
+      :scrollPerPage="false"
+      perPage="5"
+    >
+      <slide
+        class="slide"
+        v-for="item in nbWeeks"
+        :key="item.week"
+      >
+        <v-card>
+          <v-card class="week-card">
+            <v-card-text>{{ item.week }}</v-card-text>
+          </v-card>
+          <v-divider/>
+          <v-card class="week-stats-card">
+            <v-card-text>{{ item.stats }}</v-card-text>
+          </v-card>
+        </v-card>
+      </slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      nbWeeks: Array(20).fill(true).map((item, index) => ({ week: 'S' + index, stats: '50%' })),
+    }
+  },
+
+  props: {
+    nameCenter: String,
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.carousel {
+  border: 1px solid black;
+}
+.slide {
+  height: 8em;
+}
+.week-card {
+  height: 4em;
+  color: red;
+}
+.week-stats-card {
+  height: 4em;
+  color: green;
+}
+</style>
