5e0e3dac9e7f863b840fa11b38f13e7a89c79c34
Stanislas Ormières <stan@stormier.ninja>
2019-04-12 15:40:49 +0200

    Update dependencies

--- client/src/stories/common.stories.js
+++ client/src/stories/common.stories.js
@@ -3,28 +3,6 @@ import { storiesOf } from '@storybook/vue'
 
 import AppVersion from '../components/AppVersion.vue'
 
-import hexagon from '@/assets/images/hexagon.svg'
-import Hexagon from '@/components/Hexagon.vue'
-
-storiesOf('Common/Hexagon', module)
-  .add('Naked', () => ({
-    data () {
-      return { hexagon }
-    },
-    template: `${hexagon}`,
-  }))
-  .add('Dressed 1', () => ({
-    components: {
-      Hexagon,
-    },
-    template: `<v-toolbar dark><hexagon value="75" /></v-toolbar>`,
-  }))
-  .add('Dressed 2', () => ({
-    components: {
-      Hexagon,
-    },
-    template: `<v-toolbar dark><div class="c-two-hexagons"><hexagon value="75" :active="true" /><hexagon value="93" /></div></v-toolbar>`,
-  }))
 storiesOf('Common/AppVersion', module)
   .add('Basic', () => ({
     components: { AppVersion },
