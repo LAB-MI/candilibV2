6b591e730ae8f4f0c393b5e1c7814ebf18123703
Stanislas Ormières <stan@stormier.ninja>
2019-04-17 12:23:09 +0200

    Use stylus instead of scss

--- client/src/components/Hexagon.vue
+++ client/src/components/Hexagon.vue
@@ -32,7 +32,7 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="stylus" scoped>
 .icon {
   position: absolute;
   top: 0;
--- client/src/views/admin/components/DepartementSelector.vue
+++ client/src/views/admin/components/DepartementSelector.vue
@@ -40,7 +40,7 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="stylus" scoped>
 .c-two-hexagons {
   position: relative;
   width: 3em;
