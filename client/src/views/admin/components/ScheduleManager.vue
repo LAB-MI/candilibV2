62845bfebd3e5dfcc9998c2d7d512587fcef1377
jclaudan <jclaudan@student.42.fr>
2019-04-23 11:33:10 +0200

    Remove ScheduleInspecteur to separate this branch #252

--- client/src/views/admin/components/ScheduleManager.vue
+++ client/src/views/admin/components/ScheduleManager.vue
@@ -1,16 +1,36 @@
 <template>
   <div>
-    <!-- Gestion Plannings -->
-    <schedule-inspector/>
+    <v-card>
+      <v-switch
+        v-model="isImportPlacesActive"
+        :label="`Import places`"
+      ></v-switch>
+      <v-layout row wrap v-if="isImportPlacesActive">
+        <v-flex xs3>
+            <admin-import-places/>
+        </v-flex>
+        <v-spacer/>
+        <v-flex xs6>
+          <import-places-validation/>
+        </v-flex>
+      </v-layout>
+    </v-card>
   </div>
 </template>
 
 <script>
-import ScheduleInspector from './ScheduleInspector.vue'
+import AdminImportPlaces from './AdminImportPlaces.vue'
+import ImportPlacesValidation from './ImportPlacesValidation.vue'
 
 export default {
   components: {
-    ScheduleInspector,
+    AdminImportPlaces,
+    ImportPlacesValidation,
+  },
+  data () {
+    return {
+      isImportPlacesActive: false,
+    }
   },
 }
 </script>
