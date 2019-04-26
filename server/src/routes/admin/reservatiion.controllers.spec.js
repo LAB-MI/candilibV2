111b4ab4521debf4ad05b56ce19cc46bacfb39fb
baudoin.tran-econocom <baudoin.tran@econocom.com>
2019-04-26 18:01:48 +0200

    unit test to remove booking by admin

---
+++ server/src/routes/admin/reservatiion.controllers.spec.js
@@ -0,0 +1,65 @@
+import request from 'supertest'
+
+import { connect, disconnect } from '../../mongo-connection'
+
+import app, { apiPrefix } from '../../app'
+import { createPlaces } from '../../models/__tests__/places'
+import { RESA_NO_BOOKED, RESA_BOOKED_CANCEL } from './message.constants'
+import { createCentres } from '../../models/__tests__/centres'
+import { createCandidats } from '../../models/__tests__/candidats'
+import { makeResas } from '../../models/__tests__/reservations'
+import { createUser } from '../../models/user'
+
+const deleteData = elt => {
+  return elt.remove()
+}
+const email = 'test@example.com'
+const password = 'S3cr3757uff!'
+const deps = ['75', '93']
+
+jest.mock('../business/send-mail')
+jest.mock('../middlewares/verify-token')
+jest.mock('../../util/logger')
+describe('delete reservation by admin', () => {
+  let placesCreated
+  let candidatsCreated
+  let centresCreated
+  let admin
+  beforeAll(async () => {
+    await connect()
+    admin = await createUser(email, password, deps)
+    centresCreated = await createCentres()
+    placesCreated = await createPlaces()
+    candidatsCreated = await createCandidats()
+    await makeResas()
+    require('../middlewares/verify-token').__setIdAdmin(admin._id, deps)
+  })
+
+  afterAll(async () => {
+    await Promise.all(placesCreated.map(deleteData))
+    await Promise.all(centresCreated.map(deleteData))
+    await Promise.all(candidatsCreated.map(deleteData))
+    await disconnect()
+  })
+
+  it('should 400 when a place has not booked', async () => {
+    const placeSelected = placesCreated[4]
+    const { body } = await request(app)
+      .delete(`${apiPrefix}/admin/reservations/${placeSelected._id}`)
+      .set('Accept', 'application/json')
+      .expect(400)
+    expect(body).toBeDefined()
+    expect(body).toHaveProperty('success', false)
+    expect(body).toHaveProperty('message', RESA_NO_BOOKED)
+  })
+  it('should 200 when a place booked', async () => {
+    const placeSelected = placesCreated[0]
+    const { body } = await request(app)
+      .delete(`${apiPrefix}/admin/reservations/${placeSelected._id}`)
+      .set('Accept', 'application/json')
+      .expect(200)
+    expect(body).toBeDefined()
+    expect(body).toHaveProperty('success', true)
+    expect(body).toHaveProperty('message', RESA_BOOKED_CANCEL)
+  })
+})
