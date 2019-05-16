19ce1317adb5a8720b175111f553b2ff972e8649
Caroline Robillard <line.robillard@gmail.com>
2019-05-16 11:12:45 +0200

    :pencil2: Fix formulation mail annulation #309

--- server/src/routes/business/mail/cancel-booking-by-admin-template.js
+++ server/src/routes/business/mail/cancel-booking-by-admin-template.js
@@ -9,7 +9,7 @@ export const getCancelBookingByAdminTemplate = (
 ) => `<p>Madame, Monsieur ${nomNaissance},</p>
 <br>
 <p>
-  Votre réservation à ${nameCentre} le ${dateResa} à ${houreResa} avec le numéro NEPH ${codeNeph} a été annulée suite à un imprévu qui incombe à l'administration.
+  Votre réservation à ${nameCentre} le ${dateResa} à ${houreResa} avec le numéro NEPH ${codeNeph} a été annulée suite à un imprévu lié à l'administration.
   Vous serez contacté dans les prochains jours pour replanifier un créneau à votre convenance.
   Vous pouvez aussi solliciter <a href=${urlFAQ}>le service administratif dédié</a> dès à présent.
 </p>
