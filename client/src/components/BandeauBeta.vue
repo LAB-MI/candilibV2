53f6a14e7896fbde2667a4772141e3bb1b3c9385
Caroline Robillard <line.robillard@gmail.com>
2019-06-06 11:50:41 +0200

    :sparkles: Add bandeau bêta

---
+++ client/src/components/BandeauBeta.vue
@@ -0,0 +1,36 @@
+<template>
+  <img :src="beta" alt="Version bêta" :class="{beta: true, absolute}" />
+</template>
+
+<script>
+import beta from '@/assets/images/beta.png'
+
+export default {
+  props: {
+    absolute: Boolean,
+  },
+  data () {
+    return {
+      beta,
+    }
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+
+.beta {
+  height: 4.2em;
+
+  &.absolute {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+
+  @media (max-width: 767px) {
+    height: 4em;
+  }
+}
+
+</style>
