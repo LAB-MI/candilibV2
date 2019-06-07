225b190fb0267d775542a556f5e5c28a9154a1ec
baudoin.tran-econocom <baudoin.tran@econocom.com>
2019-06-07 17:51:11 +0200

    fix log format for admin

--- server/src/routes/admin/admin.controllers.js
+++ server/src/routes/admin/admin.controllers.js
@@ -1,9 +1,36 @@
 import { findUserById } from '../../models/user'
+import { appLogger } from '../../util'
 
 export const getMe = async (req, res) => {
-  const { email, departements } = await findUserById(req.userId)
-  res.json({
-    email,
-    departements,
-  })
+  const loggerInfo = {
+    section: 'admin-me',
+    action: 'get-me',
+    user: req.userId,
+  }
+  appLogger.info(loggerInfo)
+  try {
+    const { email, departements } = await findUserById(req.userId)
+    appLogger.debug({
+      ...loggerInfo,
+      results: { email, departements },
+    })
+    if (!email || !departements) {
+      const error = new Error('Utilisateur non trouvé')
+      throw error
+    }
+    return res.json({
+      email,
+      departements,
+    })
+  } catch (error) {
+    appLogger.error({
+      ...loggerInfo,
+      message: error.message,
+      error,
+    })
+    res.status(401).send({
+      message: 'Accès interdit',
+      success: false,
+    })
+  }
 }
