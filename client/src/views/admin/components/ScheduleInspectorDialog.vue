384705fdfd9000465dd85c92ca6b55290d8a4e32
Caroline Robillard <line.robillard@gmail.com>
2019-06-14 15:27:40 +0200

    Disposer du nom x neph du candidat sur un créneau réservé en survolant l'icone #439

--- client/src/views/admin/components/ScheduleInspectorDialog.vue
+++ client/src/views/admin/components/ScheduleInspectorDialog.vue
@@ -2,22 +2,44 @@
   <td class="text-xs-right">
     <div class="text-xs-center"
     >
+      <v-tooltip bottom>
+        <span v-show="isLoadingCandidat">
+          Chargement...
+        </span>
+        <span v-show="!isLoadingCandidat">
+          <span v-show="place && place.candidat">
+            {{ candidat.prenom }}
+            {{ candidat.nomNaissance }} |
+            {{ candidat.codeNeph }}
+          </span>
+
+          <span v-show="place && !place.candidat && place.inspecteur">
+            place disponible
+          </span>
+          <span v-show="!place">
+            place indisponible
+          </span>
+        </span>
+
+          <template v-slot:activator="{ on }">
+            <v-btn
+              color="white"
+              dark
+              v-on="on"
+              @mouseover="fetchCandidat"
+              @click="getCandidat"
+            >
+              <v-icon :color="color">
+                {{ icon }}
+              </v-icon>
+            </v-btn>
+          </template>
+      </v-tooltip>
+
       <v-dialog
         v-model="dialog"
         width="650"
       >
-        <template v-slot:activator="{ on }">
-          <v-btn
-            color="white"
-            dark
-            v-on="on"
-            @click="getCandidat"
-          >
-            <v-icon :color="color">
-              {{ icon }}
-            </v-icon>
-          </v-btn>
-        </template>
         <shedule-inspector-dialog-content
           :closeDialog="closeDialog"
           :flagModal="flagModal"
@@ -34,6 +56,8 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 import SheduleInspectorDialogContent from './SheduleInspectorDialogContent.vue'
 import { FETCH_CANDIDAT_REQUEST } from '@/store'
 
@@ -48,6 +72,8 @@ export default {
       dialog: false,
       icon: '',
       flagModal: undefined,
+      tooltip: false,
+      isLoadingCandidat: false,
     }
   },
 
@@ -60,6 +86,9 @@ export default {
   },
 
   computed: {
+    ...mapState({
+      candidat: (state) => state.candidats.candidat || {},
+    }),
     place () {
       return this.content.place
     },
@@ -85,11 +114,18 @@ export default {
     },
 
     getCandidat () {
+      this.dialog = true
+      this.fetchCandidat()
+    },
+
+    async fetchCandidat () {
+      this.isLoadingCandidat = true
       const candidatId = this.place && this.place.candidat
       const departement = this.$store.state.admin.departements.active
       if (candidatId) {
-        this.$store.dispatch(FETCH_CANDIDAT_REQUEST, { candidatId, departement })
+        await this.$store.dispatch(FETCH_CANDIDAT_REQUEST, { candidatId, departement })
       }
+      this.isLoadingCandidat = false
     },
 
     closeDialog () {
