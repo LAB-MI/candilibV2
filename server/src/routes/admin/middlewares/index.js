28d2e8bf280777e9e9db313fc0c81816530bd878
baudoin.tran-econocom <baudoin.tran@econocom.com>
2019-06-20 14:23:29 +0200

    api middleware verify access tech for aurige

--- server/src/routes/admin/middlewares/index.js
+++ server/src/routes/admin/middlewares/index.js
@@ -1,2 +1,4 @@
 export * from './verify-admin-level'
 export * from './verify-admin-departement'
+export * from './verify-tech-level'
+export * from './verify-access-aurige'
--- server/src/routes/admin/middlewares/verify-access-aurige.js
+++ server/src/routes/admin/middlewares/verify-access-aurige.js
@@ -1,18 +1,26 @@
 import config from '../../../config'
 import { appLogger } from '../../../util'
 
-export function verifyTechLevel (req, res, next) {
-  const userLevel = req.userLevel
+export function verifyAccessAurige (req, res, next) {
+  const { userLevel, query } = req
+  const { for: requestedAction } = query
+
   const loggerInfo = {
     section: 'admin-token',
-    action: 'check-level',
+    action: 'check-access-aurige',
     user: req.userId,
+    userLevel,
+    requestedAction,
   }
+  appLogger.debug({ ...loggerInfo })
   try {
-    if (userLevel >= config.userStatusLevels.tech) {
-      return next()
+    if (
+      requestedAction === 'aurige' &&
+      userLevel < config.userStatusLevels.admin
+    ) {
+      throw new Error('Accès interdit')
     }
-    throw new Error('Accès interdit')
+    return next()
   } catch (err) {
     appLogger.error({
       ...loggerInfo,
