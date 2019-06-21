2606fa168b4391fc811a6104477ef98f29a71221
Caroline Robillard <line.robillard@gmail.com>
2019-06-21 15:23:39 +0200

    :lipstick: ameliorer css search input

--- client/src/views/admin/components/HomeAdminPage.vue
+++ client/src/views/admin/components/HomeAdminPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="u-flex  u-flex--center fixe-input">
+    <div class="u-flex  u-flex--center  u-flex--v-start">
       <search-candidat class="search-input"/>
       <search-inspecteur class="search-input" />
     </div>
@@ -42,12 +42,6 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
-.fixe-input {
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-}
-
 .search-input {
   margin: 1em 1em;
 }
