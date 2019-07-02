e335245d10a7dfca36070afa39bb4ef63853ff37
Stanislas Ormières <stan@stormier.ninja>
2019-07-02 15:08:20 +0200

    :white_check_mark: Fix tests on date

--- client/src/util/frenchDateTime.js
+++ client/src/util/frenchDateTime.js
@@ -4,37 +4,34 @@ export const FRENCH_TIME_ZONE = 'Europe/Paris'
 
 export const frenchLocaleZone = { locale: 'fr', zone: FRENCH_TIME_ZONE }
 
-export const getFrenchLuxonDateFromIso = (isoDate) => {
-  return isoDate && DateTime.fromISO(isoDate).setLocale('fr').setZone(FRENCH_TIME_ZONE)
+export const getFrenchLuxonDateFromIso = isoDate => {
+  return (
+    isoDate &&
+    DateTime.fromISO(isoDate)
+      .setLocale('fr')
+      .setZone(FRENCH_TIME_ZONE)
+  )
 }
 
-export const getDateTimeFrFromJSDate = date =>
-  DateTime.fromJSDate(date, { locale: 'fr', zone: FRENCH_TIME_ZONE })
+export const getDateTimeFrFromJSDate = date => DateTime.fromJSDate(date, { locale: 'fr', zone: FRENCH_TIME_ZONE })
 
 export const getFrenchDateFromLuxon = dateTime =>
-  dateTime && dateTime.setLocale('fr').setZone(FRENCH_TIME_ZONE).toLocaleString({
-    weekday: 'long',
-    month: 'long',
-    day: '2-digit',
-    year: 'numeric',
-  })
+  dateTime &&
+  dateTime
+    .setLocale('fr')
+    .setZone(FRENCH_TIME_ZONE)
+    .toLocaleString({
+      weekday: 'long',
+      month: 'long',
+      day: '2-digit',
+      year: 'numeric',
+    })
 
 export const getFrenchDateTimeFromLuxon = dateTime =>
-  dateTime && dateTime.setLocale('fr').setZone(FRENCH_TIME_ZONE).toLocaleString({
-    weekday: 'long',
-    month: 'long',
-    day: '2-digit',
-    year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-  })
-
-export const getFrenchDateFromIso = (isoDate) => {
-  return isoDate && getFrenchDateFromLuxon(getFrenchLuxonDateFromIso(isoDate))
-}
-
-export const getFrenchDateTimeFromIso = (isoDate) => {
-  return isoDate && getFrenchLuxonDateFromIso(isoDate)
+  dateTime &&
+  dateTime
+    .setLocale('fr')
+    .setZone(FRENCH_TIME_ZONE)
     .toLocaleString({
       weekday: 'long',
       month: 'long',
@@ -43,14 +40,44 @@ export const getFrenchDateTimeFromIso = (isoDate) => {
       hour: '2-digit',
       minute: '2-digit',
     })
+
+export const getFrenchDateFromIso = isoDate => {
+  return isoDate && getFrenchDateFromLuxon(getFrenchLuxonDateFromIso(isoDate))
 }
 
-export const getFrenchLuxonDateTimeFromSql = (sqlDate) => sqlDate && DateTime.fromSQL(sqlDate, frenchLocaleZone)
+export const getFrenchDateTimeFromIso = isoDate => {
+  return (
+    isoDate &&
+    getFrenchLuxonDateFromIso(isoDate).toLocaleString({
+      weekday: 'long',
+      month: 'long',
+      day: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+    })
+  )
+}
 
-export const getFrenchLuxonCurrentDateTime = () => DateTime.local().setLocale('fr').setZone(FRENCH_TIME_ZONE)
+export const getFrenchLuxonDateTimeFromSql = sqlDate => sqlDate && DateTime.fromSQL(sqlDate, frenchLocaleZone)
 
-export const getFrenchLuxonDateTime = (...args) => DateTime.local(...args).setLocale('fr').setZone(FRENCH_TIME_ZONE)
+export const getFrenchLuxonCurrentDateTime = () =>
+  DateTime.local()
+    .setLocale('fr')
+    .setZone(FRENCH_TIME_ZONE)
 
-export const getFrenchLuxonDateFromObject = (obj) => obj && DateTime.fromObject(obj, frenchLocaleZone)
+export const getFrenchLuxonDateTime = (...args) =>
+  DateTime.local(...args)
+    .setLocale('fr')
+    .setZone(FRENCH_TIME_ZONE)
 
-export const getFrenchWeeksInWeekYear = (year) => DateTime.local(year).setLocale('fr').setZone(FRENCH_TIME_ZONE).weeksInWeekYear
+export const getFrenchLuxonDateFromObject = obj =>
+  obj &&
+  DateTime.fromObject(obj)
+    .setLocale('fr')
+    .setZone(FRENCH_TIME_ZONE)
+
+export const getFrenchWeeksInWeekYear = year =>
+  DateTime.local(year)
+    .setLocale('fr')
+    .setZone(FRENCH_TIME_ZONE).weeksInWeekYear
