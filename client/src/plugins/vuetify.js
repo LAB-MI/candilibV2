3ade416c548280fa4b480c484dd63f2edafd7866
Stanislas Ormières <stan@stormier.ninja>
2019-08-12 17:40:48 +0200

    :zap: Improve fonts loading
    - Roboto is now self-hosted
    - Material icons are now self-hosted

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'typeface-roboto/index.css'
 import 'vuetify/dist/vuetify.min.css'
 import fr from 'vuetify/es5/locale/fr'
 
