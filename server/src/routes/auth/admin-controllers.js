c10ff60c9b56cb095d6b1747c9af8e731800c197
Robillardca <line.robillard@gmail.com>
2019-09-17 17:26:23 +0200

    Implementer route api

--- server/src/routes/auth/admin-controllers.js
+++ server/src/routes/auth/admin-controllers.js
@@ -3,7 +3,7 @@
  * @module
  */
 import { createToken, appLogger } from '../../util'
-import { findUserByCredentials } from '../../models/user'
+import { findUserByCredentials, findUserById } from '../../models/user'
 
 /**
  * @typedef {Object} BadCredentialsBody
@@ -50,7 +50,7 @@ export const getAdminToken = async (req, res) => {
         action: 'FAILED_TO_FIND_USER_BY_EMAIL',
         description: `${email} not in DB`,
       })
-      return res.status(401).send(badCredentialsBody)
+      return res.status(401).json(badCredentialsBody)
     }
 
     const isValidCredentials = user.comparePassword(password)
@@ -60,7 +60,7 @@ export const getAdminToken = async (req, res) => {
         ...loggerInfo,
         action: 'USER_GAVE_WRONG_PASSWORD',
       })
-      return res.status(401).send(badCredentialsBody)
+      return res.status(401).json(badCredentialsBody)
     }
 
     const token = createToken(user._id, user.status, user.departements)
@@ -71,7 +71,7 @@ export const getAdminToken = async (req, res) => {
       complement: user.status,
     })
 
-    return res.status(201).send({ success: true, token })
+    return res.status(201).json({ success: true, token })
   } catch (error) {
     appLogger.error({
       ...loggerInfo,
@@ -79,9 +79,43 @@ export const getAdminToken = async (req, res) => {
       description: error.message,
       error,
     })
-    return res.status(500).send({
+    return res.status(500).json({
       message: `Erreur serveur : ${error.message}`,
       success: false,
     })
   }
+}
+
+
+export const resetMyPassword = async (req, res)=> {
+  //récuperer le nouveau mot de passe
+  const newPassword = req.body.newPassword
+  //récuperer la confirmation du nouveau mot de passe
+  const confirmNewPassword = req.body.confirmNewPassword
+  //vérifier que le nouveau mot de passe et la confiramtion son identique
+  if (newPassword !== confirmNewPassword) {
+    return res.json({
+      success: false,
+      message: 'Oups! Les mots de passe ne correspondent pas.'
+    })
+  }
+  //récuperer l'utilisateur
+  const user = await findUserById(req.userId)
+  //récupérer l'ancien mot de passe
+  const oldPassword = req.body.oldPassword
+  //vérifier ancien mot de passe = au mot de passe BDD
+  const isValidCredentials = user.comparePassword(password)
+
+  if (!isValidCredentials) {
+    appLogger.warn({
+      ...loggerInfo,
+      action: 'USER_GAVE_WRONG_PASSWORD',
+    })
+    return res.status(401).json(badCredentialsBody)
+  }
+  //si tout est bon mettre à jour avec le nouveau mot de passe
+  if (oldPassword) {
+    return 'le nouveau mot de passe à été pris en compte'
+  }
+
 }
