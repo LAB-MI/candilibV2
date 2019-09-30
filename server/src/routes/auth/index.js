6316e0164de925cdf0a8acd869c7007d27f0ef6e
Robillardca <line.robillard@gmail.com>
2019-09-30 14:49:04 +0200

    Ajouter bouton réinitialiser le mot de passe

--- server/src/routes/auth/index.js
+++ server/src/routes/auth/index.js
@@ -151,6 +151,46 @@ const router = express.Router()
  */
 router.post('/admin/reset-link', requestPasswdReset)
 
+/**
+ * @swagger
+ *
+ * /admin/reset-link:
+ *   post:
+ *     summary: Envoyer un mail de demande réinitialisation
+ *     description: Envoi d'un lien de réinitialisation de mot de passe
+ *     produces:
+ *       - application/json
+ *     security:
+ *       - bearerAuth: []
+ *
+ *     responses:
+ *       200:
+ *         description:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               allOf:
+ *                 - $ref: '#/components/schemas/InfoObject'
+ *                 - example:
+ *                     success: true
+ *                     message: Un courriel vient de vous être envoyé sur email@example.com
+ *
+ *       404:
+ *         $ref: '#/components/responses/InvalidEmailResponse'
+ *
+ *       500:
+ *         $ref: '#/components/responses/UnknownErrorResponse'
+ *
+ */
+
+/**
+ *
+ *
+ * @callback requestPasswdReset
+ * @see {@link http://localhost:8000/api-docs/#/default/post_admin_reset-link}
+ */
+router.post('/admin/reset-link', requestPasswdReset)
+
 router.post('/admin/token', getAdminToken)
 
 /**
