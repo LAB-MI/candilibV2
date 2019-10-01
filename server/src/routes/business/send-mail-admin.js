7497f842edc0738db485548a79c0180adb6286c1
Stanislas Ormières <stan@stormier.ninja>
2019-10-01 17:09:28 +0200

    :pencil: Add JSDoc

--- server/src/routes/business/send-mail-admin.js
+++ server/src/routes/business/send-mail-admin.js
@@ -1,3 +1,9 @@
+/**
+ * Ensemble des fonctions pour envoyer les mails aux répartiteurs
+ *
+ * @module business/send-mail-admin
+ */
+
 import { sendMail } from './send-mail'
 import { getHtmlBody } from './mail'
 import config from '../../config'
@@ -10,27 +16,63 @@ export const getUrlResetLink = async email => {
   )}&hash=${encodeURIComponent(emailValidationHash)}`
 }
 
+/**
+ * @function
+ *
+ * @param {string} email Adresse courriel de l'utilisateur
+ */
 export const sendMailResetLink = async email => {
   const urlResetLink = await getUrlResetLink(email)
   const mail = await getResetLinkMail(urlResetLink)
-  return sendMail(email, mail)
+  await sendMail(email, mail)
 }
 
+/**
+ * @function
+ *
+ * @param {string} urlResetLink Lien à envoyer dans le mail avec l'adresse courriel et le hash de réinitialisation
+ *
+ * @returns {string} Contenu HTML de la partie informative du contenu du mail de réinitialisation du mot de passe
+ */
 export const getResetLinkTemplate = urlResetLink => `
-  <p>Vous avez demandé à réinitialiser votre mot de passe. Vous pouvez dès à present 
-  réinitialiser votre mot de passe en cliquant sur le lien suivant : <a href='${urlResetLink}'>réinitialiser le mot de passe</a>
+  <p>
+    Vous avez demandé à réinitialiser votre mot de passe. Vous pouvez dès à present
+    réinitialiser votre mot de passe en cliquant sur le lien suivant :
+    a href='${urlResetLink}'>réinitialiser le mot de passe</a>
   </p>
   <br>
   <p align="right">L'équipe Candilib</p>
 `
 
+/**
+ * @function
+ *
+ * @param {string} urlResetLink Lien à envoyer dans le mail avec l'adresse courriel et le hash de réinitialisation
+ *
+ * @returns {string} Contenu HTML de tout le contenu du mail de réinitialisation du mot de passe
+ */
 export const getResetLinkMailBody = urlResetLink => {
   const body = getResetLinkTemplate(urlResetLink)
   return getHtmlBody(body)
 }
 
+/**
+ *
+ * @function
+ *
+ * @param {string} urlResetLink Lien à envoyer dans le mail avec l'adresse courriel et le hash de réinitialisation
+ *
+ * @returns {MailData} Titre et corps du mail à envoyer pour la réinitialisation du mot de passe
+ */
 export const getResetLinkMail = urlResetLink => {
   const subject = 'Réinitialisation de votre mot de passe'
   const content = getResetLinkMailBody(urlResetLink)
   return { subject, content }
 }
+
+/**
+ *
+ * @typedef {Object} MailData
+ * @property {string} subject - Titre de l'email
+ * @property {string} content - Corps de l'email
+ */
