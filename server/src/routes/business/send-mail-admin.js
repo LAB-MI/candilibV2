f3a6db535c3d7027c1552a48218e448fce241bfe
Caroline Robillard <line.robillard@gmail.com>
2019-10-09 13:48:31 +0200

    Create variable Admin_URL for link email

--- server/src/routes/business/send-mail-admin.js
+++ server/src/routes/business/send-mail-admin.js
@@ -11,7 +11,7 @@ import { addEmailValidationHash } from '../../models/user'
 
 export const getUrlResetLink = async email => {
   const emailValidationHash = await addEmailValidationHash(email)
-  return `${config.PUBLIC_URL}/reset-link?email=${encodeURIComponent(
+  return `${config.ADMIN_URL}/reset-link?email=${encodeURIComponent(
     email
   )}&hash=${encodeURIComponent(emailValidationHash)}`
 }
