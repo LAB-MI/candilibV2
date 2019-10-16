9d2a5c62b1018954f7025a4a76271d3a94c7854f
pli01 <pli01@users.noreply.github.com>
2019-10-16 09:44:01 +0200

    Config scheduler: Add MONGO_URL

--- manon/src/get-agenda.js
+++ manon/src/get-agenda.js
@@ -4,9 +4,9 @@ import getConfig from './config.js'
 function getAgenda () {
   const config = getConfig()
 
-  const { dbUser, dbPass, dbName, agendaCollectionName } = config.db
+  const { dbUser, dbPass, dbName, mongoUrl, agendaCollectionName } = config.db
 
-  const mongoConnectionString = `mongodb://${dbUser}:${dbPass}@localhost:27017/${dbName}`
+  const mongoConnectionString = mongoUrl || `mongodb://${dbUser}:${dbPass}@localhost:27017/${dbName}`
 
   const agenda = new Agenda({
     db: { address: mongoConnectionString, collection: agendaCollectionName },
