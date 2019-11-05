3f1ce611e123f0f4d93db70026e38292d082d231
pli01 <pli01@users.noreply.github.com>
2019-11-05 11:40:20 +0100

    Add tests up scheduler Default scheduler config (disable define and schedule) README

--- tests/test-up-scheduler.sh
+++ tests/test-up-scheduler.sh
@@ -20,7 +20,8 @@ until [ "$timeout" -le 0 -o "$test_result" -eq "0" ] ; do
 	${DC} -f ${DC_APP_SCHEDULER_RUN_PROD} exec ${DC_USE_TTY} $container_name bash -c 'nodejs - <<EOF
 var MongoClient = require("mongodb").MongoClient;
 var url = process.env.MONGO_URL;
-MongoClient.connect(url,{ useNewUrlParser: true } , function(err, dbo) {   if (err) throw err; dbo.db("candilib").collection("SchedulerJobs").find({},{ _id: 0, name: 1}).toArray(function(err, result) { if (err ) throw err; if (result.length === 0) { console.log("jobs empty") ; process.exit(1) ; } console.log("jobs:"+result.length); dbo.close();  }); });
+MongoClient.connect(url,{ useNewUrlParser: true } , function(err, dbo) {   if (err) throw err; dbo.db("candilib").listCollections({name: "SchedulerJobs"}).next(function(err, collinfo) {if (!collinfo) {console.log("Collection SchedulerJobs introuvable!"); process.exit(1); } console.log("Collection SchedulerJobs OK"); }); dbo.close();   });
+MongoClient.connect(url,{ useNewUrlParser: true } , function(err, dbo) {   if (err) throw err; dbo.db("candilib").collection("SchedulerJobs").find({},{ _id: 0, name: 1}).toArray(function(err, result) { if (err ) throw err; if (result.length === 0) { console.log("jobs list empty") ; process.exit(0) ; } console.log("jobs:"+result.length); dbo.close();  }); });
 EOF
 '
 	test_result=$?
