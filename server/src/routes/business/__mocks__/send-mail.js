9eb28b4fd0e0a74e00832a02f1ab5c74e0993a6f
archibald.perez@isep.fr <archibald.perez@isep.fr>
2019-11-20 16:50:28 +0100

    :white_check_mark: Add unit test for send bordereaux

--- server/src/routes/business/__mocks__/send-mail.js
+++ server/src/routes/business/__mocks__/send-mail.js
@@ -1,13 +1,16 @@
 import getMailData from '../message-templates'
 
+let mails = []
 let mail
-export const __initMail = () => {
-  mail = undefined
-}
 
+export const getMails = () => mails
 export const getMail = () => mail
 
+export const __initMail = () => (mail = undefined)
+export const deleteMails = () => (mails = [])
+
 export const sendMail = async (to, { subject, content: html } = {}) => {
+  mails.push({ to, subject, html })
   mail = { to, subject, html }
   return true
 }
