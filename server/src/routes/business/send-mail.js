41a3727732ad765f164aaf6b33e88eb8b8e033b8
jclaudan <jclaudan@student.42.fr>
2019-11-29 18:13:03 +0100

    :construction: Add functional action

--- server/src/routes/business/send-mail.js
+++ server/src/routes/business/send-mail.js
@@ -4,7 +4,13 @@ import { htmlToText } from 'nodemailer-html-to-text'
 
 import getMailData from './message-templates'
 import config, { smtpOptions } from '../../config'
-import { techLogger, appLogger } from '../../util'
+import {
+  appLogger,
+  createToken,
+  getFrenchLuxonFromJSDate,
+  techLogger,
+  getFrenchLuxon,
+} from '../../util'
 
 export const sendMail = async (to, { subject, content: html }) => {
   const transporter = nodemailer.createTransport(smtpTransport(smtpOptions))
@@ -46,12 +52,24 @@ export const sendMailToAccount = async (candidat, flag) => {
   return sendMail(candidat.email, message)
 }
 
-export const sendMagicLink = async (candidat, token) => {
+export const sendMagicLink = async candidat => {
   const flag = 'CHECK_OK'
-  const authUrl = `${config.PUBLIC_URL}${config.CANDIDAT_ROUTE}`
 
-  const url = `${authUrl}?token=${encodeURIComponent(token)}`
+  const url = getUrl(candidat)
 
   const message = await getMailData(candidat, flag, url)
   return sendMail(candidat.email, message)
+}
+
+function getUrl (candidat) {
+  const candidatAccessDate = getFrenchLuxonFromJSDate(candidat.canAccessAt)
+  const dateNow = getFrenchLuxon()
+
+  if (!candidatAccessDate || dateNow > candidatAccessDate) {
+    const token = createToken(candidat.id, config.userStatuses.CANDIDAT)
+    const authUrl = `${config.PUBLIC_URL}${config.CANDIDAT_ROUTE}`
+    return `${authUrl}?token=${encodeURIComponent(token)}`
+  }
+
+  return null
 }
