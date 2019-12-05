ffc36a35eb7cef2ee68cae7537a3de0668513280
archibald.perez@isep.fr <archibald.perez@isep.fr>
2019-12-05 11:51:20 +0100

    Fix logger section

--- server/src/routes/candidat/candidat-config-controller.js
+++ server/src/routes/candidat/candidat-config-controller.js
@@ -3,6 +3,9 @@ import { getCandidatConfigBusiness } from './business/candidat-config-business'
 
 export async function getCandidatConfig (req, res) {
   const config = getCandidatConfigBusiness()
-  appLogger.info({ description: 'Getting candidat config', section: 'config' })
+  appLogger.info({
+    description: 'Getting candidat config',
+    section: 'candidat-config',
+  })
   res.json({ success: true, config })
 }
