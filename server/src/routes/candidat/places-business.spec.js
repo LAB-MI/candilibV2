2c5656b44410c6dbd44b774b4ebb0f1741c0b0d2
baudoin.tran-econocom <baudoin.tran@econocom.com>
2019-12-17 18:12:51 +0100

    fix unit test to get dates places availables which are before ETG Expired only

--- server/src/routes/candidat/places-business.spec.js
+++ server/src/routes/candidat/places-business.spec.js
@@ -1,16 +1,23 @@
-import { connect, disconnect } from '../../mongo-connection'
-
-import {
-  createCentres,
-  removeCentres,
-  createPlaces,
-  removePlaces,
-  centres,
-} from '../../models/__tests__/'
-import { getDatesByCentre, canCancelReservation } from './places-business'
-import { getFrenchLuxon } from '../../util'
 import config from '../../config'
 import { findCandidatById } from '../../models/candidat'
+import { findCentreByNameAndDepartement } from '../../models/centre'
+import { createPlace } from '../../models/place'
+import {
+  centres,
+  createCentres,
+  createPlaces,
+  removeCentres,
+  removePlaces,
+} from '../../models/__tests__/'
+import { connect, disconnect } from '../../mongo-connection'
+import { getFrenchFormattedDateTime, getFrenchLuxon } from '../../util'
+import { NB_YEARS_ETG_EXPIRED } from '../common/constants'
+import { CANDIDAT_DATE_ETG_KO } from './message.constants'
+import {
+  canCancelReservation,
+  getDatesByCentre,
+  getDatesByCentreId,
+} from './places-business'
 
 jest.mock('../../models/candidat')
 
@@ -37,10 +44,54 @@ describe('Test places business: utiles functions', () => {
 })
 
 describe('Test places business: get dates from places available', () => {
+  let placesCreated
+  let centreSelected
+  let nbPlacesAvaibles
+  let dateIn3Months
   beforeAll(async () => {
     await connect()
-    await createCentres()
-    await createPlaces()
+    const centresCreated = await createCentres()
+    placesCreated = await createPlaces()
+    centreSelected = centresCreated.find(
+      ({ nom, departement }) =>
+        nom === centres[1].nom && departement === centres[1].departement
+    )
+    const placesCreatedFromSelected = placesCreated.filter(
+      ({ centre }) => centre === centreSelected._id
+    )
+    const inspecteur = placesCreatedFromSelected[0].inspecteur
+
+    dateIn3Months = getFrenchLuxon().plus({
+      month: 3,
+    })
+    const count = (await Promise.all([
+      createPlace({
+        centre: centreSelected._id,
+        inspecteur,
+        date: getFrenchLuxon().plus({
+          month: 1,
+        }),
+      }),
+      createPlace({
+        centre: centreSelected._id,
+        inspecteur,
+        date: getFrenchLuxon().plus({ month: 2 }),
+      }),
+      createPlace({
+        centre: centreSelected._id,
+        inspecteur,
+        date: dateIn3Months,
+      }),
+      createPlace({
+        centre: centreSelected._id,
+        inspecteur,
+        date: getFrenchLuxon().plus({
+          month: 4, // config.numberOfVisibleMonths + 1
+        }),
+      }),
+    ])).length
+
+    nbPlacesAvaibles = placesCreatedFromSelected.length + count - 1
   })
 
   afterAll(async () => {
@@ -60,5 +111,66 @@ describe('Test places business: get dates from places available', () => {
       centreSelected.nom
     )
     expect(dates).toBeDefined()
+    expect(dates).toHaveLength(nbPlacesAvaibles)
+  })
+
+  it('Should get any places from Centre2 when ETG expired now', async () => {
+    const dateETGExpired = getFrenchLuxon()
+    findCandidatById.mockResolvedValue({
+      dateReussiteETG: dateETGExpired
+        .minus({ years: NB_YEARS_ETG_EXPIRED })
+        .toJSDate(),
+    })
+    const centreSelected = await findCentreByNameAndDepartement(
+      centres[1].nom,
+      centres[1].departement
+    )
+    const begin = getFrenchLuxon().toISODate()
+    const end = getFrenchLuxon()
+      .plus({ years: 1 })
+      .toISODate()
+
+    try {
+      const dates = await getDatesByCentreId(
+        centreSelected._id,
+        begin,
+        end,
+        'candidatId'
+      )
+      expect(dates).toBeUndefined()
+    } catch (error) {
+      expect(error).toHaveProperty('status', 400)
+      expect(error).toHaveProperty(
+        'message',
+        CANDIDAT_DATE_ETG_KO + getFrenchFormattedDateTime(dateETGExpired).date
+      )
+    }
+  })
+
+  it('Should get many places from Centre2 when ETG expired in 2 months', async () => {
+    const dateETGExpired = getFrenchLuxon().plus({ months: 2 })
+    findCandidatById.mockResolvedValue({
+      dateReussiteETG: dateETGExpired
+        .minus({ years: NB_YEARS_ETG_EXPIRED })
+        .toJSDate(),
+    })
+    const centreSelected = await findCentreByNameAndDepartement(
+      centres[1].nom,
+      centres[1].departement
+    )
+    const begin = getFrenchLuxon().toISODate()
+    const end = getFrenchLuxon()
+      .plus({ months: 6 })
+      .toISODate()
+
+    const dates = await getDatesByCentreId(
+      centreSelected._id.toString(),
+      begin,
+      end,
+      'candidatId'
+    )
+    expect(dates).toBeDefined()
+    expect(dates).toHaveLength(nbPlacesAvaibles - 1)
+    expect(dates.includes(dateIn3Months.toISO()))
   })
 })
