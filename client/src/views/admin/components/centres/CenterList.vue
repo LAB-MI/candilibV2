327a298dd5f5ea9d3b1160410046bcf9b40db8e3
archibald.perez@isep.fr <archibald.perez@isep.fr>
2019-12-19 12:06:45 +0100

    Use v-data-table for listing centres

--- client/src/views/admin/components/centres/CenterList.vue
+++ client/src/views/admin/components/centres/CenterList.vue
@@ -8,57 +8,70 @@
         </h3>
         <center-create />
       </v-card>
-      <v-divider />
 
-      <v-card>
+      <v-card class="mt-4">
         <h3 class="text-xs-center">
           Liste des centres
         </h3>
-        <v-list
+        <v-data-table
           class="centre-grid"
+          :headers="headers"
+          :items="centres"
+          must-sort
+          :sort-by="['departement', 'nom']"
         >
-          <v-list-item
-            v-for="centre in centres"
-            :key="centre._id"
-            selectable
-            :class="{ 'blue-grey  lighten-5': !centre.active }"
+          <template
+            v-slot:header.departement
+            class="pa-0"
+          />
+          <template
+            v-slot:item.nom="{ item }"
           >
-            <v-card-text>
-              <div class="u-flex">
-                <v-list-item-content>
-                  <v-list-item-title>
-                    <span class="u-uppercase">
-                      {{ centre.nom }}
-                      ({{ centre.departement }})
-                    </span>
-                  </v-list-item-title>
+            <div class="table-column  u-flex">
+              <v-list-item-content>
+                <v-list-item-title>
+                  <span class="u-uppercase">
+                    {{ item.nom }}
+                  </span>
+                </v-list-item-title>
 
-                  <v-list-item-subtitle>
-                    {{ centre.adresse }}
-                  </v-list-item-subtitle>
-                </v-list-item-content>
-                <a
-                  target="_blank"
-                  class="location-icon  u-flex"
-                  :href="href(centre.geoloc.coordinates)"
-                  @click.stop="() => true"
+                <v-list-item-subtitle
+                  class="blue-grey--text"
                 >
-                  <v-icon>
-                    location_on
-                  </v-icon>
-                </a>
-              </div>
-            </v-card-text>
-            <!--
-            <center-list-dialog
-              :centre="centre.nom + ' (' + centre.departement + ')'"
-              :action="centre.active ? 'Désactiver' : 'Activer'"
-              :active="centre.active"
-              @click="changeState(centre._id, !centre.active)"
-            />
-            -->
-          </v-list-item>
-        </v-list>
+                  {{ item.adresse }}
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </div>
+          </template>
+          <template
+            v-slot:item.geoloc="{ item }"
+          >
+            <a
+              target="_blank"
+              class="table-column  u-flex"
+              :href="href(item.geoloc.coordinates)"
+              @click.stop="() => true"
+            >
+              <v-icon>
+                location_on
+              </v-icon>
+            </a>
+          </template>
+          <!--
+          <template
+            v-slot:item.active="{ item }"
+          >
+            <div class="table-column">
+              <center-list-dialog
+                :centre="item.nom + ' (' + item.departement + ')'"
+                :action="item.active ? 'Désactiver' : 'Activer'"
+                :active="item.active"
+                @click="changeState(item._id, !item.active)"
+              />
+            </div>
+          </template>
+          -->
+        </v-data-table>
       </v-card>
     </v-container>
   </div>
@@ -74,6 +87,18 @@ export default {
     centerCreate,
     // centerListDialog,
   },
+
+  data () {
+    return {
+      headers: [
+        { text: '', value: 'departement', align: 'center' },
+        { text: 'Centre', value: 'nom' },
+        { text: '', value: 'geoloc', sortable: false, align: 'center' },
+        // { text: 'Actions', value: 'active', align: 'center' },
+      ],
+    }
+  },
+
   computed: {
     centres () {
       return this.$store.state.admin.centres.list
@@ -97,8 +122,8 @@ export default {
 }
 </script>
 
-<style lang="postcss" scoped>
-.location-icon {
+<style lang="stylus" scoped>
+.table-column {
   border-left: 1px solid rgba(150, 150, 150, 0.5);
   height: 3em;
   padding: 0 1.5em;
@@ -110,6 +135,10 @@ export default {
   grid-template-columns: 1fr;
   width: 100%;
   width: auto;
+
+  >>> td {
+    padding: 0;
+  }
 }
 
 h3 {
