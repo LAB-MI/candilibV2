cfee07417ce473fef96a13378c926330ca2d3ff1
jclaudan <jclaudan@student.42.fr>
2020-01-23 16:55:53 +0100

    :white_check_mark: fix test in candidateLogin

--- server/dev-setup/create-candidats.js
+++ server/dev-setup/create-candidats.js
@@ -3,79 +3,38 @@ import { createCandidat } from '../src/models/candidat'
 import { simpleLogger as logger } from '../src/util'
 
 import candidats from './candidats'
-// import candidatsValid from './candidatsValid'
+import candidatsCypress from './candidatsCypress'
 
-logger.info('Creating candidats')
-
-const createValidCandidats = async () => {
-  const candidats = [
-    {
-      codeNeph: '61234567898',
-      prenom: 'CC_FRONT',
-      nomNaissance: 'CANDIDAT_FRONT',
-      adresse: '40 Avenue des terroirs de France 75012 Paris',
-      portable: '0676543986',
-      email: 'candidat_front@candi.lib',
-      departement: '75',
-      isEvaluationDone: false,
-      isValidatedEmail: true,
-      isValidatedByAurige: true,
-    },
-    {
-      codeNeph: '61234567899',
-      prenom: 'CC_INTERACTIVE',
-      nomNaissance: 'CANDIDAT_INTERACTIVE',
-      adresse: '40 Avenue des terroirs de France 75012 Paris',
-      portable: '0676543986',
-      email: 'candidat_interactive@candi.lib',
-      departement: '75',
-      isEvaluationDone: false,
-      isValidatedEmail: true,
-      isValidatedByAurige: true,
-    },
-    {
-      codeNeph: '61234567897',
-      prenom: 'CC_DELAY_AFTER_FAILURE',
-      nomNaissance: 'CANDIDAT_DELAY_AFTER_FAILURE',
-      adresse: '40 Avenue des terroirs de France 75012 Paris',
-      portable: '0676543986',
-      email: 'candidat_delay_after_failure@candi.lib',
-      departement: '75',
-      isEvaluationDone: true,
-      isValidatedEmail: true,
-      isValidatedByAurige: true,
-    },
-    {
-      codeNeph: '61234567896',
-      prenom: 'CC_STATS_KPI',
-      nomNaissance: 'CANDIDAT_STATS_KPI',
-      adresse: '40 Avenue des terroirs de France 75012 Paris',
-      portable: '0676543986',
-      email: 'candidat_stats_kpi@candi.lib',
-      departement: '75',
-      isEvaluationDone: false,
-      isValidatedEmail: true,
-      isValidatedByAurige: true,
-    },
-  ]
-  candidats.map(async candidat => {
-    const candidatCreated = await createCandidat(candidat)
-    candidatCreated.isValidatedEmail = candidat.isValidatedEmail
-    candidatCreated.isValidatedByAurige = candidat.isValidatedByAurige
-    candidatCreated.isEvaluationDone = candidat.isEvaluationDone
-    candidatCreated.dateReussiteETG = candidat.dateReussiteETG || new Date()
-    return candidatCreated.save()
-  })
-}
-
-export default async () => {
-  await createValidCandidats()
+export default () => {
   const candidatsCreated = candidats.map(candidat =>
     createCandidat(candidat)
       .then(() => {
-        logger.info(`Compte candidat ${candidat.codeNeph} créé !`)
+        logger.info(`Compte candidat ${candidat.codeNeph} - ${candidat.email}  créé !`)
       })
       .catch(error => logger.error(error))
   )
-  return Promise.all(candidatsCreated)
+
+  const candidatsCypressCreated = candidatsCypress.map(candidat => {
+    const isValidatedEmail = candidat.isValidatedEmail
+    const isValidatedByAurige = candidat.isValidatedByAurige
+    const isEvaluationDone = candidat.isEvaluationDone
+    const dateReussiteETG = candidat.dateReussiteETG
+    return createCandidat(candidat)
+      .then(async candidatCreated => {
+        candidatCreated.isValidatedEmail = isValidatedEmail
+        candidatCreated.isValidatedByAurige = isValidatedByAurige
+        candidatCreated.isEvaluationDone = isEvaluationDone
+        candidatCreated.dateReussiteETG = dateReussiteETG || new Date()
+        try {
+          await candidatCreated.save()
+          logger.info(`Compte candidat ${candidatCreated.codeNeph} - ${candidatCreated.email}  créé !`)
+        } catch (error) {
+          logger.error(error)
+        }
+      })
+      .catch(error => logger.error(error))
+  })
+
+  const fusionOfCandidats = candidatsCypressCreated.concat(candidatsCreated)
+  return Promise.all(fusionOfCandidats)
 }
