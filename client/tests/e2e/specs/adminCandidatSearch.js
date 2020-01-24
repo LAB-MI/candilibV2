f6c3f7c74db51c588b495e1a87f6b047c31a0413
Robillardca <line.robillard@gmail.com>
2020-01-24 11:58:46 +0100

    :ok_hand: Avoid ducplicate in e2e tests
    - Use beforeEach and afterEach

--- client/tests/e2e/specs/adminCandidatSearch.js
+++ client/tests/e2e/specs/adminCandidatSearch.js
@@ -11,9 +11,17 @@ describe('Search Candidate', () => {
     cy.adminDisconnection()
     cy.candidatePreSignUp()
   })
-  it('Searches for candidat', () => {
+
+  beforeEach(() => {
     cy.adminLogin()
     cy.visit(Cypress.env('frontAdmin') + 'admin/admin-candidat')
+  })
+
+  afterEach(() => {
+    cy.adminDisconnection()
+  })
+
+  it('Searches for candidat', () => {
     cy.get('.t-search-candidat [type=text]').type(Cypress.env('candidat'))
     cy.contains(Cypress.env('candidat')).click()
     cy.get('h3').should('contain', 'nformations')
@@ -21,12 +29,9 @@ describe('Search Candidate', () => {
       .contains('Email')
       .parent()
       .should('contain', Cypress.env('emailCandidat'))
-    cy.adminDisconnection()
   })
 
   it('Search candidate by filter', () => {
-    cy.adminLogin()
-    cy.visit(Cypress.env('frontAdmin') + 'admin/admin-candidat')
     cy.get('.t-checkbox-one')
       .parent()
       .click()
