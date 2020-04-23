342f4d09514033f9e6978dac7d85f4e85be15905
baudoin.tran-econocom <baudoin.tran@econocom.com>
2020-04-23 10:28:22 +0200

    :recycle: extract layout from landing page

---
+++ client/src/views/candidat/components/LayoutLandingPage.vue
@@ -0,0 +1,80 @@
+<template>
+  <section>
+    <div class="text--center">
+      <already-signed-up
+        btn-class-name="t-already-signed-up-button-top"
+        test-class-suffix="top"
+      />
+      <router-link :to="{ name: 'candidat-presignup' }">
+        <v-btn color="success">
+          Se pré-inscrire
+        </v-btn>
+      </router-link>
+    </div>
+
+    <slot />
+    <footer>
+      <div class="actions">
+        <already-signed-up
+          btn-class-name="t-already-signed-up-button-bottom"
+          test-class-suffix="bottom"
+        />
+        <router-link :to="{ name: 'candidat-presignup' }">
+          <v-btn color="success">
+            Se pré-inscrire
+          </v-btn>
+        </router-link>
+
+        <router-link
+          :to="{ name: 'faq' }"
+          class="t-faq"
+        >
+          <v-btn
+            text
+            color="#fff"
+            tag="a"
+            :to="{ name: 'faq' }"
+            tabindex="10"
+          >
+            Une Question ?
+          </v-btn>
+        </router-link>
+
+        <router-link
+          :to="{ name: 'mentions-legales' }"
+          class="t-mentions-legales"
+        >
+          <v-btn
+            text
+            color="#fff"
+            tag="a"
+            :to="{ name: 'mentions-legales' }"
+            tabindex="9"
+          >
+            Mentions Légales
+          </v-btn>
+        </router-link>
+      </div>
+    </footer>
+  </section>
+</template>
+
+<script>
+import AlreadySignedUp from './AlreadySignedUp'
+
+export default {
+  components: {
+    AlreadySignedUp,
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+
+.actions {
+  display: grid;
+  justify-content: center;
+  grid-template-columns: auto auto;
+  justify-items: center;
+}
+</style>
