ed9cfad22605727fc04006e2f903340e7bf7bb93
baudoin.tran-econocom <baudoin.tran@econocom.com>
2020-05-04 18:13:46 +0200

    :sparkles: add template mail contactus send to admin

---
+++ server/src/routes/business/mail/contact-us-for-admin.js
@@ -0,0 +1,26 @@
+export const getContactUsForAdmin = (
+  codeNeph,
+  nomNaissance,
+  prenom,
+  email,
+  portable,
+  homeDepartement,
+  hasSignUp,
+  subject,
+  message
+) => `<p>
+  <strong>Infromation Candidat:</strong><br>
+  <ul>
+  <li>  NEPH: ${codeNeph}</li>
+    <li>  Nom: ${nomNaissance}</li>
+    <li>  Prénom: ${prenom}</li>
+    <li>  Courriel: <a href="mailto:${email}?subject=RE:${subject}">${email}</a></li>
+    <li>  Portable: ${portable}</li>
+    <li>  Département de résidence: ${homeDepartement}</li>
+    <li>  Candidat pré-inscrit ou inscrit: ${hasSignUp}</li>
+  </ul>
+  </p>
+  <p>
+  <strong>Message: </strong>
+  ${message}
+  </p>`
