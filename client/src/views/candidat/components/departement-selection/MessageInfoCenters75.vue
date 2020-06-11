9756664d9dac0d5e1e0fd74e34dcfd18e54f1677
baudoin.tran-econocom <baudoin.tran@econocom.com>
2020-06-11 18:11:20 +0200

    :sparkles: Message centers from 75

---
+++ client/src/views/candidat/components/departement-selection/MessageInfoCenters75.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-alert
+    v-if="centers && centers.length"
+    type="info"
+    class="rounded-corner"
+  >
+    {{ message }}
+  </v-alert>
+</template>
+
+<script>
+import { FETCH_PARIS_CENTERS_REQUEST } from '@/store'
+
+import { NB_FOR_CENTERS_75 } from '../faq/FaqJson'
+import { mapState } from 'vuex'
+export default {
+  components: {
+  },
+  computed: {
+    ...mapState({
+      centers (state) { return state.parisCenters.list },
+    }),
+    message () { return NB_FOR_CENTERS_75(this.centers) },
+  },
+  mounted () {
+    this.$store.dispatch(FETCH_PARIS_CENTERS_REQUEST)
+  },
+}
+</script>
