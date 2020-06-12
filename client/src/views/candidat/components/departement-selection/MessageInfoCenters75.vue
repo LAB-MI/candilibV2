ae81be01a37ac8f6963b40bf0a152530ccc6ac3f
baudoin.tran-econocom <baudoin.tran@econocom.com>
2020-06-12 16:57:11 +0200

    :sparkles: update message for 75 with centers in uniq department

--- client/src/views/candidat/components/departement-selection/MessageInfoCenters75.vue
+++ client/src/views/candidat/components/departement-selection/MessageInfoCenters75.vue
@@ -5,11 +5,14 @@
     class="rounded-corner"
   >
     {{ message }}
+    <div v-if="centersUniq && centersUniq.length">
+      {{ messageUniq }}
+    </div>
   </v-alert>
 </template>
 
 <script>
-import { FETCH_PARIS_CENTERS_REQUEST } from '@/store'
+import { FETCH_PARIS_CENTERS_REQUEST, FETCH_PARIS_CENTERS_UNIQ_REQUEST } from '@/store'
 
 import { NB_FOR_CENTERS_75 } from '../faq/FaqJson'
 import { mapState } from 'vuex'
@@ -19,11 +22,18 @@ export default {
   computed: {
     ...mapState({
       centers (state) { return state.parisCenters.list },
+      centersUniq (state) { return state.parisCenters.listUniq },
     }),
-    message () { return NB_FOR_CENTERS_75(this.centers) },
+    message () {
+      return NB_FOR_CENTERS_75(this.centers)
+    },
+    messageUniq () {
+      return `Pour les centres suivants ${this.centersUniq?.join(', ')}, veuillez contacter le service Candilib 75 en cas de problème.`
+    },
   },
   mounted () {
     this.$store.dispatch(FETCH_PARIS_CENTERS_REQUEST)
+    this.$store.dispatch(FETCH_PARIS_CENTERS_UNIQ_REQUEST)
   },
 }
 </script>
