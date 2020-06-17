f90d5dea5968a223baa71303d895142874d1afae
baudoin.tran-econocom <baudoin.tran@econocom.com>
2020-06-17 10:34:13 +0200

    :sparkles: get the date to display the places for candidat

---
+++ server/src/routes/candidat/util/date-to-display.js
@@ -0,0 +1,12 @@
+import { getFrenchLuxon } from '../../../util'
+
+/**
+ * Obtenir la date pour récupérer les places créés avant cette date et quand les récupérer
+ * exemple: les places créés avant le jour J à 12h seront affiché à 12h
+ * exemple: les places créés aprés le jour J à 12h seront affiché lendemain à 12h
+ * @function
+ */
+export const getDateDisplayPlaces = () => {
+  const newDate = getFrenchLuxon().minus({ hours: 12 })
+  return newDate.set({ hour: 12, minute: 0, second: 0 })
+}
---
+++ server/src/routes/candidat/util/date-to-display.spec.js
@@ -0,0 +1,28 @@
+import { getFrenchLuxon, FRENCH_TIME_ZONE } from '../../../util'
+import { DateTime } from 'luxon'
+import { getDateDisplayPlaces } from './date-to-display'
+
+jest.mock('../../../util/date-util')
+
+const dateFct = obj =>
+  DateTime.fromObject(obj)
+    .setLocale('fr')
+    .setZone(FRENCH_TIME_ZONE)
+
+describe('Date to display places', () => {
+  it('get yesterday at 12h when now is 11h59', () => {
+    getFrenchLuxon.mockReturnValue(
+      dateFct({ hour: 11, minute: 59, second: 59 })
+    )
+    const dateToDisplay = getDateDisplayPlaces()
+    expect(dateToDisplay).toEqual(
+      dateFct({ hour: 12, minute: 0, second: 0 }).minus({ days: 1 })
+    )
+  })
+
+  it('get yesterday at 12h when now is 12h00', () => {
+    getFrenchLuxon.mockReturnValue(dateFct({ hour: 12, minute: 0, second: 0 }))
+    const dateToDisplay = getDateDisplayPlaces()
+    expect(dateToDisplay).toEqual(dateFct({ hour: 12, minute: 0, second: 0 }))
+  })
+})
