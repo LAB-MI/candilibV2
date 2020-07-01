2743edea314731a6ada45e4b6b6e6b0480f895cc
baudoin.tran-econocom <baudoin.tran@econocom.com>
2020-07-01 18:34:45 +0200

    :white_check_mark: update test:e2e for 2 new windows

--- client/tests/e2e/specs/placesAfter12h.js
+++ client/tests/e2e/specs/placesAfter12h.js
@@ -1,10 +1,11 @@
 const { now } = require('../support/dateUtils')
+
 let dateAt3Months
 let dateAt2Months
 let dateAt1Months
 let dateAt2Weeks
 describe('Display new place after 12h', () => {
-  const NbCreneauxByDay = 13 * 2
+  const NbCreneauxByDay = 15 * 2
   let magicLink
   before(() => {
     cy.daleteAllPlaces()
