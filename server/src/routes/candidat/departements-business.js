88af7bffb140b68e1fb24c9ade179f0cfed54c25
baudoin.tran-econocom <baudoin.tran@econocom.com>
2020-08-07 12:09:32 +0200

    :wip: :zap: add business count places by geoDepartements

--- server/src/routes/candidat/departements-business.js
+++ server/src/routes/candidat/departements-business.js
@@ -3,7 +3,62 @@ import { UNKNOWN_ERROR_GET_DEPARTEMENT } from '../admin/message.constants'
 
 import { findCentresWithNbPlacesByGeoDepartement } from '../common/centre-business'
 import { candidatCanReservePlaceForThisPeriod } from './util'
+import { findAllActiveCentres } from '../../models/centre'
+import { countAvailablePlacesByCentreIds } from '../../models/place'
+import { getDateDisplayPlaces } from './util/date-to-display'
 
+export const getAllGeoDepartemenntsInfos = async (userId) => {
+  const loggerContent = {
+    action: 'Getting active geo departements business',
+    section: 'candidat-departements-business',
+    userId,
+  }
+
+  try {
+    const now = Date.now()
+    const {
+      beginPeriod,
+      endPeriod,
+    } = await candidatCanReservePlaceForThisPeriod(userId)
+    console.log({ dureePeriod: Date.now() - now })
+
+    const centres = await findAllActiveCentres({ geoDepartement: 1, nom: 1 })
+
+    const groupByGeoDepartement = centres.reduce((byGeoDep, centre) => {
+      const { _id, geoDepartement } = centre
+      if (!byGeoDep[geoDepartement]) {
+        byGeoDep[geoDepartement] = []
+      }
+      byGeoDep[geoDepartement].push(centre._id)
+      return byGeoDep
+    }, {})
+
+    const geoDepartementsInfos = await Promise.all(
+      Object.keys(groupByGeoDepartement).map(async geoDep => {
+        const count = await countAvailablePlacesByCentreIds(
+          groupByGeoDepartement[geoDep],
+          beginPeriod,
+          endPeriod,
+          getDateDisplayPlaces(),
+        )
+        return {
+          geoDepartement: geoDep,
+          count,
+        }
+      })
+    )
+
+    return geoDepartementsInfos
+  } catch (error) {
+    appLogger.error({
+      ...loggerContent,
+      error,
+      description: error.message,
+    })
+
+    throw new Error(UNKNOWN_ERROR_GET_DEPARTEMENT)
+  }
+}
 // TODO: ADD JSDOC
 export const getGeoDepartementsInfos = async (geoDepartementsId, userId) => {
   const loggerContent = {
@@ -14,10 +69,14 @@ export const getGeoDepartementsInfos = async (geoDepartementsId, userId) => {
   }
 
   try {
+    const now = Date.now()
     const {
       beginPeriod,
       endPeriod,
     } = await candidatCanReservePlaceForThisPeriod(userId)
+    console.log({ dureePeriod: Date.now() - now })
+
+    const now1 = Date.now()
 
     const geoDepartementsInfos = await Promise.all(
       geoDepartementsId.map(async geoDepartement => {
@@ -40,6 +99,9 @@ export const getGeoDepartementsInfos = async (geoDepartementsId, userId) => {
         return shapedGeoDepartementInfo
       }),
     )
+
+    console.log({ geoDepartementsInfos: Date.now() - now1 })
+
     return geoDepartementsInfos
   } catch (error) {
     appLogger.error({
