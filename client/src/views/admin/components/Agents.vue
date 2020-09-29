a7f98b9e2ad373d4f36801fbe5f14799b15e0172
jclaudan <jclaudan@student.42.fr>
2020-09-29 15:50:55 +0200

    :sparkles: Refactor update users component

--- client/src/views/admin/components/Agents.vue
+++ client/src/views/admin/components/Agents.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
     <CandilibUsers />
+    <CandilibArchivedUsers />
     <CandilibIpcsr />
   </div>
 </template>
 
 <script>
 import CandilibUsers from './repartiteurDelegue/Users.vue'
+import CandilibArchivedUsers from './repartiteurDelegue/ArchivedUsers.vue'
 import CandilibIpcsr from './ipcsr/Ipcsr.vue'
 
 export default {
+  name: 'Agents',
+
   components: {
     CandilibUsers,
+    CandilibArchivedUsers,
     CandilibIpcsr,
   },
 }
