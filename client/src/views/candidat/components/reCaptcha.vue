dd8e50f7b9f0bb827bc3213c7ba2a56e2137b067
baudoin.tran-econocom <baudoin.tran@econocom.com>
2020-10-09 09:37:09 +0200

    component recaptcha

---
+++ client/src/views/candidat/components/reCaptcha.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="recaptcha">
+    Execute recaptcha
+  </button>
+</template>
+
+<script>
+import Vue from 'vue'
+import { VueReCaptcha } from 'vue-recaptcha-v3'
+
+// For more options see below
+Vue.use(VueReCaptcha, { siteKey: '6LdBKNUZAAAAADsERd8aXNxbYgWYM9GUY-V4pic-' })
+
+export default {
+  methods: {
+    async recaptcha () {
+      // (optional) Wait until recaptcha has been loaded.
+      await this.$recaptchaLoaded()
+
+      // Execute reCAPTCHA with action "login".
+      //   const token = await this.$recaptcha('login')
+
+      // Do stuff with the received token.
+    },
+  },
+}
+
+</script>
