449b675a01a073a8bc3cf16624d542187e15f5a0
jclaudan <jclaudan@student.42.fr>
2020-11-02 12:34:29 +0100

    :whale: Update docker compose front and api

--- client/src/plugins/reCaptchaGoogle.js
+++ client/src/plugins/reCaptchaGoogle.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
 import { VueReCaptcha } from 'vue-recaptcha-v3'
 
-Vue.use(VueReCaptcha, { siteKey: process.env.RECAPTCHA_SITE_KEY || '6Ld7YtwZAAAAAM_wYRBp4qdQQR9FfggZLie00Ubj' })
+Vue.use(VueReCaptcha, { siteKey: process.env.RECAPTCHA_SITE_KEY || '' })
