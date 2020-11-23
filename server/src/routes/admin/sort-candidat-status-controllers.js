ec2cc1478fb3b16273a1603fb314c46c971ed1dd
jclaudan <jclaudan@student.42.fr>
2020-11-23 09:51:36 +0100

    :ok_hand: Update status candilib controller and route

--- server/src/routes/admin/sort-candidat-status-controllers.js
+++ server/src/routes/admin/sort-candidat-status-controllers.js
@@ -1,13 +1,36 @@
+import { appLogger } from '../../util'
 import { sortStatus } from './sort-candidat-status-business'
 
 // TODO: JSDOC
 export const sortStatusCandilib = async (req, res) => {
+  const loggerInfo = {
+    section: 'admin-sort-status-candilib',
+    admin: req.userId,
+  }
+
   try {
-    await sortStatus()
-    console.log('OKOKOK!!')
-    res.status(200)
+    const message = 'Mise à jour des status éffectués'
+    const summary = await sortStatus()
+    appLogger.info({
+      ...loggerInfo,
+      description: message,
+      summary,
+    })
+
+    res.status(200).send({
+      success: true,
+      message,
+    })
   } catch (error) {
-    console.log({ error })
-    res.status(400)
+    appLogger.error({
+      ...loggerInfo,
+      description: error.message,
+      error,
+    })
+
+    res.status(500).send({
+      success: false,
+      message: error.message,
+    })
   }
 }
