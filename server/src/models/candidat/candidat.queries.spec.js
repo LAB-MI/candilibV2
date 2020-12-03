ded0900b96ec95a1c6f86eb133c267e1a95494d0
baudoin.tran-econocom <baudoin.tran@econocom.com>
2020-12-03 15:46:49 +0100

    split unit test candidat

--- server/src/models/candidat/candidat.queries.spec.js
+++ server/src/models/candidat/candidat.queries.spec.js
@@ -12,7 +12,6 @@ import {
   createCentres,
   createPlaces,
   deleteCandidats,
-  generateCandidats,
   makeResas,
   removeCentres,
   removePlaces,
@@ -28,9 +27,7 @@ import {
 } from './'
 import {
   archivePlace,
-  countCandidatsByStatus,
   findCandidatsMatching,
-  sortCandilibStatus,
   updateCandidatFailed,
   updateCandidatNoReussite,
 } from './candidat.queries'
@@ -63,72 +60,6 @@ describe('Candidat', () => {
 
   afterAll(async () => {
     await disconnect()
-  })
-
-  it('sort status candidats', async () => {
-    const sortableCandidat = {
-      nbCandidats: 7,
-      isValidateAurige: true,
-      isValideEmail: true,
-      canBookFrom: null,
-      canAccessAt: null,
-    }
-    const sortableCandidatCanBookInPast = {
-      nbCandidats: 1,
-      isValidateAurige: true,
-      isValideEmail: true,
-      canBookFrom: 'past',
-      canAccessAt: null,
-    }
-    const notSortableCandidatCanBookInFuture = {
-      nbCandidats: 1,
-      isValidateAurige: true,
-      isValideEmail: true,
-      canBookFrom: 'future',
-      canAccessAt: null,
-    }
-    const sortableCandidatCanAccessInFuture = {
-      nbCandidats: 1,
-      isValidateAurige: true,
-      isValideEmail: true,
-      canBookFrom: null,
-      canAccessAt: 'future',
-    }
-    const notSortableCandidatwithNothing = {
-      nbCandidats: 1,
-      isValidateAurige: false,
-      isValideEmail: true,
-      canBookFrom: null,
-      canAccessAt: null,
-    }
-
-    const allTest = [
-      sortableCandidat,
-      sortableCandidatCanBookInPast,
-      notSortableCandidatCanBookInFuture,
-      sortableCandidatCanAccessInFuture,
-      notSortableCandidatwithNothing,
-    ]
-    const data = await generateCandidats(allTest)
-
-    const createdCandidat = await Promise.all(data.map(
-      el => {
-        return createCandidat(el)
-      },
-    ))
-
-    await sortCandilibStatus()
-
-    const expectedCandidatByStatus = ['1', '1', '1', '1', '1', '5']
-    await Promise.all(expectedCandidatByStatus.map(
-      async (el, index) => {
-        const countStatus = await countCandidatsByStatus(`${index}`)
-        expect(countStatus).toBe(Number(el))
-        return countStatus
-      },
-    ))
-
-    await Promise.all(createdCandidat.map(candidat => deleteCandidat(candidat).catch(() => true)))
   })
 
   it('Find 2 candidats', async () => {
