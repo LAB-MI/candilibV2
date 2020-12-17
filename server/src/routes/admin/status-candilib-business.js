b103a7f2ee4c6afbff522376bebd07b174deb605
jclaudan <jclaudan@student.42.fr>
2020-12-17 02:04:54 +0100

    :sparkles: Add in api borne infos status

--- server/src/routes/admin/status-candilib-business.js
+++ server/src/routes/admin/status-candilib-business.js
@@ -1,6 +1,7 @@
 import { findStatusByType, upsertStatusByType } from '../../models/status'
 
 const AURIGE_STATUS = 'AURIGE_STATUS'
+const LAST_INFO_BORNE_STATUS = 'LAST_INFO_BORNE_STATUS'
 
 export const getLastSyncAurigeDateTime = () => {
   return findStatusByType({ type: AURIGE_STATUS })
@@ -8,4 +9,11 @@ export const getLastSyncAurigeDateTime = () => {
 
 export const upsertLastSyncAurige = message => {
   return upsertStatusByType({ type: AURIGE_STATUS, message })
+}
+
+export const getLastInfosBornesStatus = () => {
+  return findStatusByType({ type: LAST_INFO_BORNE_STATUS })
+}
+export const upsertLastInfosBormeStatus = infosBorneStatus => {
+  return upsertStatusByType({ type: LAST_INFO_BORNE_STATUS, message: infosBorneStatus })
 }
--- server/src/routes/admin/status-candilib-controllers.js
+++ server/src/routes/admin/status-candilib-controllers.js
@@ -3,7 +3,7 @@
  * @module routes/admin/status-candilib-controllers
  */
 import { appLogger } from '../../util/logger'
-import { getLastSyncAurigeDateTime } from './status-candilib-business'
+import { getLastInfosBornesStatus, getLastSyncAurigeDateTime } from './status-candilib-business'
 
 /**
  * Récupération de la date de la dernière synchro Aurige
@@ -54,6 +54,78 @@ export const getInfoLastSyncAurige = async (req, res) => {
         aurigeInfo: {
           date: updatedAt,
           message,
+        },
+      })
+    }
+    return res.status(400).send({
+      success: false,
+      message,
+    })
+  } catch (error) {
+    appLogger.error({
+      ...loggerInfo,
+      description: error.message,
+      error: error,
+    })
+    return res.status(500).send({ success: false, message: error.message })
+  }
+}
+
+/**
+ * Récupération des borne tous les status
+ * @async
+ * @function
+ *
+ * @param {import('express').Request} req
+ * @param {string} req.userId Id de l'utilisateur
+ * @param {import('express').Response} res
+ */
+export const getLastInfosBorneStatus = async (req, res) => {
+  const loggerInfo = {
+    section: 'admin-last-infos-bornes-status',
+    action: 'getLastInfosBorneStatus',
+    admin: req.userId,
+  }
+
+  appLogger.info({
+    ...loggerInfo,
+  })
+  try {
+    const lastInfosBornesStatus = await getLastInfosBornesStatus()
+    const message = 'Les bornes de statuts ne sont pas encore définies.'
+
+    if (lastInfosBornesStatus) {
+      const { type, message, updatedAt } = lastInfosBornesStatus
+      if (!type) {
+        appLogger.info({
+          ...loggerInfo,
+          type,
+        })
+        return res.status(400).send({
+          success: false,
+          message,
+        })
+      }
+      appLogger.info({
+        ...loggerInfo,
+        success: true,
+        borneStatusInfos: {
+          type,
+          message,
+          dateTime: updatedAt,
+        },
+      })
+      const infosBorneStatus = Object.entries((JSON.parse(message))).map(([status, infos]) => {
+        return {
+          status,
+          infos,
+        }
+      })
+      return res.status(200).send({
+        success: true,
+        borneStatusInfos: {
+          infosBorneStatus,
+          date: updatedAt,
         },
       })
     }
