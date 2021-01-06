2eccfcb3ce2599aad2f23295830886c79c4fc7ae
pli01 <pli01@noreply.github.com>
2021-01-06 17:10:20 +0100

    Use docker login if present

--- ci/docker-access.sh
+++ ci/docker-access.sh
@@ -1,8 +1,5 @@
 #!/bin/bash 
 set -e
-if [ -z "${DOCKER_PWD}" -a -z "${DOCKER_LOGIN}" ] ; then 
-  echo "DOCKER_PWD et DOCKER_LOGIN vide"
-  exit 1
+if [ -n "${DOCKER_PWD}" -a -n "${DOCKER_LOGIN}" ] ; then
+  echo "$DOCKER_PWD" | docker login --username $DOCKER_LOGIN --password-stdin
 fi
-echo "Docker access"
-echo $DOCKER_PWD | docker login --username $DOCKER_LOGIN --password-stdin
