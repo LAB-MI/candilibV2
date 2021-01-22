bf7c02a2f3e9595cb06f4dfdc291d111efd8c1df
baudointran <baudoin.tran@econocom.com>
2021-01-22 17:00:56 +0100

    Update Mail absent for new penalty

--- server/src/routes/business/mail/epreuve-pratique-absent-template.js
+++ server/src/routes/business/mail/epreuve-pratique-absent-template.js
@@ -7,10 +7,10 @@ export const getAbsentAtExamTemplate = (
 ) => `<p>Madame, Monsieur ${nameCandidat},</p>
 <br>
 <p>
-Vous avez été absent à l'examen, et nous avons perdu une place qui aurait pu bénéficier à un autre candidat. 
+Vous avez été absent à l'examen, et nous avons perdu une place qui aurait pu bénéficier à un autre candidat en attente. 
 </p>
 <p>
-Vous conserverez votre accès à Candilib, mais il vous sera appliqué une pénalité de ${timeoutToRetry} jours de délais avant de pouvoir effectuer <a href=${urlRESA}>une nouvelle réservation</a>.
+Vous conserverez votre accès à Candilib, mais il vous est appliqué un délai de présentation de ${timeoutToRetry} jours à partir de la date de l'examen où vous ne vous êtes pas présenté.
 </p>
 <p>
 Pour toute information, vous pouvez consulter <a href=${urlFAQ}>notre aide en ligne</a>.
