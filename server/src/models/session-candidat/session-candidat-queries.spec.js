1a3be69933e047b5a3a83e6cd945104fad3d5f33
jclaudan <jclaudan@student.42.fr>
2021-03-16 11:07:13 +0100

    :white_check_mark: Add test unit captcha and session

--- server/src/models/session-candidat/session-candidat-queries.spec.js
+++ server/src/models/session-candidat/session-candidat-queries.spec.js
@@ -1,9 +1,12 @@
 import {
   getSessionByCandidatId,
   createSession,
+  deleteSessionByCandidatId,
+  updateSession,
 } from './session-candidat-queries'
 import { connect, disconnect } from '../../mongo-connection'
 import { createCandidat } from '../candidat'
+import { getFrenchLuxon, getFrenchLuxonFromISO } from '../../util'
 
 const validEmail = 'candidat@example.com'
 const portable = '0612345678'
@@ -12,8 +15,43 @@ const nomNaissance = 'Dupont'
 const prenom = 'Jean'
 const codeNeph = '123456789012'
 
+// TODO: Find the best solution for export and share with other test
+const expectedCommonSession = (expectedSession, sessionToHave) => {
+  expect(expectedSession).toHaveProperty('session')
+  expect(expectedSession).toHaveProperty('userId', sessionToHave.userId)
+}
+
+const expectedExtraSession = (expectedSession, sessionToHave, optionToCheck) => {
+  const { isCount, isSessionCaptcha, isCanRetryAt, isCaptchaExpireAt, isExpires } = optionToCheck
+  expectedCommonSession(expectedSession, sessionToHave)
+
+  if (isCount) {
+    expect(expectedSession).toHaveProperty('count', sessionToHave.count)
+  }
+
+  if (isSessionCaptcha) {
+    expect(expectedSession).toHaveProperty('session', sessionToHave.session)
+    expect(expectedSession.session).toHaveProperty('key', sessionToHave.session.key)
+  }
+
+  if (isCanRetryAt) {
+    expect(expectedSession).toHaveProperty('canRetryAt', getFrenchLuxonFromISO(sessionToHave.canRetryAt).toJSDate())
+  }
+
+  if (isCaptchaExpireAt) {
+    expect(expectedSession).toHaveProperty('captchaExpireAt', getFrenchLuxonFromISO(sessionToHave.captchaExpireAt).toJSDate())
+  }
+
+  if (isExpires) {
+    expect(expectedSession).toHaveProperty('expires', getFrenchLuxonFromISO(sessionToHave.expires).toJSDate())
+  }
+}
+
 describe('SessionCandidat', () => {
   let candidat1
+  let sessionFake
+  const dateNow = getFrenchLuxon()
+
   beforeAll(async () => {
     await connect()
 
@@ -25,6 +63,18 @@ describe('SessionCandidat', () => {
       portable,
       adresse,
     })
+
+    sessionFake = {
+      userId: candidat1._id,
+      session: JSON.stringify({ key: 'value' }),
+      expires: dateNow.toISO(),
+      captchaExpireAt: dateNow.toISO(),
+      count: 0,
+    }
+  })
+
+  afterEach(async () => {
+    await deleteSessionByCandidatId(candidat1._id)
   })
 
   afterAll(async () => {
@@ -32,23 +82,42 @@ describe('SessionCandidat', () => {
   })
 
   it('should save an session candidat', async () => {
-    // Given
-    const sessionFake = {
+    const savedSessionCandidat = await createSession(sessionFake)
+    const optionToCheck = {
+      isCount: true,
+      isSessionCaptcha: true,
+      isCanRetryAt: false,
+      isCaptchaExpireAt: true,
+      isExpires: true,
+    }
+    expectedExtraSession(savedSessionCandidat, sessionFake, optionToCheck)
+  })
+
+  it('should update an session candidat', async () => {
+    const sessionInfo = {
       userId: candidat1._id,
-      session: JSON.stringify({ key: 'value' }),
-      expires: new Date(),
+      session: { key: 'value' },
+      count: 2,
+      expires: dateNow.endOf('day').toISO(),
+      canRetryAt: dateNow.plus({ minutes: 2 }).toISO(),
+      captchaExpireAt: dateNow.endOf('day').toISO(),
     }
 
-    try {
-      // When
-      const savedSessionCandidat = await createSession(sessionFake)
-      const sessionInfos = await getSessionByCandidatId(candidat1._id)
-      console.log({ savedSessionCandidat, sessionInfos })
-      expect(savedSessionCandidat).toHaveProperty('isNew', false)
-    } catch (error) {
-      console.log({ error })
-    }
+    await createSession(sessionFake)
+    const updatedStatus = await updateSession(sessionInfo)
 
-    // Then
+    expect(updatedStatus).toHaveProperty('nModified', 1)
+    expect(updatedStatus).toHaveProperty('n', 1)
+
+    const sessionAfterUpdate = await getSessionByCandidatId(candidat1._id)
+
+    const optionToCheck = {
+      isCount: true,
+      isSessionCaptcha: true,
+      isCanRetryAt: true,
+      isCaptchaExpireAt: true,
+      isExpires: true,
+    }
+    expectedExtraSession(sessionAfterUpdate, sessionInfo, optionToCheck)
   })
 })
