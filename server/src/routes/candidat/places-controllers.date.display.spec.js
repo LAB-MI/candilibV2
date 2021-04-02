f283cf3fb701a47985c33ddb98a002617be911e9
baudointran <baudoin.tran@econocom.com>
2021-04-02 17:55:03 +0200

    unit test with flag in recently dep

--- server/src/routes/candidat/places-controllers.date.display.spec.js
+++ server/src/routes/candidat/places-controllers.date.display.spec.js
@@ -14,7 +14,7 @@ import {
   createPlacesWithVisibleAt,
   visibleAtNow12h,
 } from '../../models/__tests__/places.date.display'
-import { getFrenchLuxonFromJSDate } from '../../util'
+import { getFrenchLuxon, getFrenchLuxonFromJSDate } from '../../util'
 import { findPlaceByCandidatId } from '../../models/place'
 import {
   setNowBefore12h,
@@ -24,6 +24,7 @@ import {
 
 import { verifyAccesPlacesByCandidat } from './middlewares/verify-candidat'
 import { candidatStatuses } from '../common/__mocks__/candidat-status-const'
+import { updateCandidatById } from '../../models/candidat'
 
 jest.mock('../../util/logger')
 require('../../util/logger').setWithConsole(false)
@@ -145,71 +146,91 @@ describe('Get places available and display at 12h', () => {
   })
 
   describe.each`
-  status | statuses | after12hExpected | after12h20Expected | after12h50Expected | selectedAfter12h | selectedAfter12h20 | selectedAfter12h50 | canBookedAfter12h | canBookedAfter12h20 | canBookedAfter12h50
-  ${undefined} | ${undefined} | ${3} | ${3} | ${3} | ${1} | ${1} | ${1} | ${''} | ${''} | ${''}
-  ${'0'} | ${candidatStatuses} | ${3} | ${3} | ${3} | ${1} | ${1} | ${1} | ${''} | ${''} | ${''}
-  ${'2'} | ${candidatStatuses} | ${1} | ${3} | ${3} | ${0} | ${1} | ${1} | ${'not'} | ${''} | ${''}
-  ${'5'} | ${candidatStatuses} | ${1} | ${1} | ${3} | ${0} | ${0} | ${1} | ${'not'} | ${'not'} | ${''}
-  ${'6'} | ${candidatStatuses} | ${1} | ${1} | ${3} | ${0} | ${0} | ${1} | ${'not'} | ${'not'} | ${''}
-`('for $status', ({ status, statuses, after12hExpected, after12h20Expected, after12h50Expected, selectedAfter12h, selectedAfter12h20, selectedAfter12h50, canBookedAfter12h, canBookedAfter12h20, canBookedAfter12h50 }) => {
-    beforeAll(() => {
-      const moduleCandidatStatuses = require('../common/candidat-status-const')
-      moduleCandidatStatuses.candidatStatuses = statuses
+  homeDept | isInRecentlyDept |  hasPenalty 
+  ${'93'}  | ${true}          |  ${false}
+  ${'75'}  | ${false}         |  ${false}
+  ${'75'}  | ${true}          |  ${false}
+  ${'75'}  | ${true}          |  ${true}
+  `('$homeDept is recently added: $isInRecentlyDept. Have penalty: $hasPenalty ', ({ homeDept, isInRecentlyDept, hasPenalty }) => {
+    const isFirstStatus = isInRecentlyDept && homeDept === '75' && !hasPenalty
+    describe.each`
+      status       | statuses            | after12hExpected         | after12h20Expected       | after12h50Expected | selectedAfter12h         | selectedAfter12h20       | selectedAfter12h50 | canBookedAfter12h             | canBookedAfter12h20           | canBookedAfter12h50
+      ${undefined} | ${undefined}        | ${3}                     | ${3}                     | ${3}               | ${1}                     | ${1}                     | ${1}               | ${''}                         | ${''}                         | ${''}
+      ${'0'}       | ${candidatStatuses} | ${3}                     | ${3}                     | ${3}               | ${1}                     | ${1}                     | ${1}               | ${''}                         | ${''}                         | ${''}
+      ${'2'}       | ${candidatStatuses} | ${isFirstStatus ? 3 : 1} | ${3}                     | ${3}               | ${isFirstStatus ? 1 : 0} | ${1}                     | ${1}               | ${isFirstStatus ? '' : 'not'} | ${''}                         | ${''}
+      ${'5'}       | ${candidatStatuses} | ${isFirstStatus ? 3 : 1} | ${isFirstStatus ? 3 : 1} | ${3}               | ${isFirstStatus ? 1 : 0} | ${isFirstStatus ? 1 : 0} | ${1}               | ${isFirstStatus ? '' : 'not'} | ${isFirstStatus ? '' : 'not'} | ${''}
+      ${'6'}       | ${candidatStatuses} | ${isFirstStatus ? 3 : 1} | ${isFirstStatus ? 3 : 1} | ${3}               | ${isFirstStatus ? 1 : 0} | ${isFirstStatus ? 1 : 0} | ${1}               | ${isFirstStatus ? '' : 'not'} | ${isFirstStatus ? '' : 'not'} | ${''}
+      `('for status:$status', ({ status, statuses, after12hExpected, after12h20Expected, after12h50Expected, selectedAfter12h, selectedAfter12h20, selectedAfter12h50, canBookedAfter12h, canBookedAfter12h20, canBookedAfter12h50 }) => {
+      beforeAll(async () => {
+        const moduleCandidatStatuses = require('../common/candidat-status-const')
+        moduleCandidatStatuses.candidatStatuses = statuses
 
-      verifyAccesPlacesByCandidat.mockImplementation((req, res, next) => {
-        req.userStatus = status
-        next()
+        await updateCandidatById(idCandidat, {
+          status,
+          canBookFrom: getFrenchLuxon().plus({ days: hasPenalty ? 1 : -1 }),
+        },
+        )
+
+        verifyAccesPlacesByCandidat.mockImplementation(async (req, res, next) => {
+          const verifyCandidat = jest.requireActual('./middlewares/verify-candidat')
+          req.isInRecentlyDept = isInRecentlyDept
+          req.candidatHomeDepartement = homeDept
+          const nextTmp = () => {
+          }
+          await verifyCandidat.verifyAccesPlacesByCandidat(req, res, nextTmp)
+          next()
+        })
       })
-    })
 
-    afterAll(() => {
-      setNowAtNow()
-    })
+      afterAll(() => {
+        setNowAtNow()
+      })
 
-    it('Should get 1 place for 75 when now is before 12h', async () => {
-      setNowBefore12h()
-      await expectedPlaces(1)
-    })
+      it('Should get 1 place for 75 when now is before 12h', async () => {
+        setNowBefore12h()
+        await expectedPlaces(1)
+      })
 
-    it.each([
-      [after12hExpected, 0],
-      [after12h20Expected, 20],
-      [after12h50Expected, 50],
-    ])(`Should get %i places for 75 when now is after 12h%i for status ${status}`, async (npPlacesExpected, minutes) => {
-      setNowAfter12h(minutes)
-      await expectedPlaces(npPlacesExpected)
-    })
+      it.each([
+        [after12hExpected, 0],
+        [after12h20Expected, 20],
+        [after12h50Expected, 50],
+      ])(`Should get %i places for 75 when now is after 12h%i for status ${status}`, async (npPlacesExpected, minutes) => {
+        setNowAfter12h(minutes)
+        await expectedPlaces(npPlacesExpected)
+      })
 
-    it.each([
-      [selectedAfter12h, 0],
-      [selectedAfter12h20, 20],
-      [selectedAfter12h50, 50],
-    ])(`Should 200 with  %i available place before 12h when it is after 12h%i by center name and geo-departement for status ${status}`, async (npPlacesExpected, minutes) => {
-      setNowAfter12h(minutes)
-      await expectedPlaceByNameCentreAndGeoDep(placesCreatedBefore.date, npPlacesExpected)
-    })
+      it.each([
+        [selectedAfter12h, 0],
+        [selectedAfter12h20, 20],
+        [selectedAfter12h50, 50],
+      ])(`Should 200 with  %i available place before 12h when it is after 12h%i by center name and geo-departement for status ${status}`, async (npPlacesExpected, minutes) => {
+        setNowAfter12h(minutes)
+        await expectedPlaceByNameCentreAndGeoDep(placesCreatedBefore.date, npPlacesExpected)
+      })
 
-    it('Should 200 with no available place before 12h when it is after 12h by center name and geo-departement', async () => {
-      setNowBefore12h()
-      await expectedPlaceByNameCentreAndGeoDep(placesCreatedBefore.date, 0)
-    })
+      it('Should 200 with no available place before 12h when it is after 12h by center name and geo-departement', async () => {
+        setNowBefore12h()
+        await expectedPlaceByNameCentreAndGeoDep(placesCreatedBefore.date, 0)
+      })
 
-    it('should not booked place by candidat with info bookedAt when it is before 12h', async () => {
-      setNowBefore12h()
-      await expectedBookedFailed(placesCreatedBefore, idCandidat)
-    })
-
-    it.each([
-      [canBookedAfter12h, 0],
-      [canBookedAfter12h20, 20],
-      [canBookedAfter12h50, 50],
-    ])('should %s booked place by candidat with info bookedAt when it is after 12h%i', async (canBooked, minutes) => {
-      setNowAfter12h(minutes)
-      if (canBooked === 'not') {
+      it('should not booked place by candidat with info bookedAt when it is before 12h', async () => {
+        setNowBefore12h()
         await expectedBookedFailed(placesCreatedBefore, idCandidat)
-      } else {
-        await expectedBooked(placesCreatedBefore, idCandidat)
-      }
+      })
+
+      it.each([
+        [canBookedAfter12h, 0],
+        [canBookedAfter12h20, 20],
+        [canBookedAfter12h50, 50],
+      ])('should %s booked place by candidat with info bookedAt when it is after 12h%i', async (canBooked, minutes) => {
+        setNowAfter12h(minutes)
+        if (canBooked === 'not') {
+          await expectedBookedFailed(placesCreatedBefore, idCandidat)
+        } else {
+          await expectedBooked(placesCreatedBefore, idCandidat)
+        }
+      })
     })
   })
 
