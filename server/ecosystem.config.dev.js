d2d81a61cb6e679c943c44d7abc6007d6bbd51a6
jclaudan <jclaudan@users.noreply.github.com>
2021-05-30 22:28:23 +0200

    :wrench: Update pm2 config prod and dev

--- server/ecosystem.config.dev.js
+++ server/ecosystem.config.dev.js
@@ -18,5 +18,26 @@ module.exports = {
     env_production: {
       NODE_ENV: 'production',
     },
-  }],
+  },
+  {
+    name: 'SDL',
+    script: 'src/schedulers/boot-dev.js',
+    // instances: 1,
+    // exec_mode: 'cluster',
+    watch: ['src/schedulers'],
+    // ignore_watch: ['*.spec.js', '**/*.spec.js'],
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    autorestart: true,
+    log_file: '/dev/null',
+    out_file: '/dev/null',
+    error_file: '/dev/null',
+    merge_logs: true,
+    env: {
+      NODE_ENV: 'development',
+    },
+    env_production: {
+      NODE_ENV: 'production',
+    },
+  }
+],
 }
--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -16,5 +16,23 @@ module.exports = {
     env_production: {
       NODE_ENV: 'production',
     },
-  }],
+  },
+  {
+    name: 'SDL',
+    script: 'dist/index.js',
+    instances: 1,
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    autorestart: true,
+    log_file: '/dev/null',
+    out_file: '/dev/null',
+    error_file: '/dev/null',
+    merge_logs: true,
+    env: {
+      NODE_ENV: 'development',
+    },
+    env_production: {
+      NODE_ENV: 'production',
+    },
+  }
+],
 }
