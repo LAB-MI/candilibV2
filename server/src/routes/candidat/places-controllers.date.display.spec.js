c873a297c60dab72118c2ac7dab7852c57aeeb44
baudointran <baudointran@users.noreply.github.com>
2021-07-09 16:55:35 +0200

    fix unit test to book places after 12h

--- server/src/routes/candidat/places-controllers.date.display.spec.js
+++ server/src/routes/candidat/places-controllers.date.display.spec.js
@@ -16,6 +16,8 @@ import {
 } from '../../models/__tests__/places.date.display'
 import { getFrenchLuxon, getFrenchLuxonFromJSDate } from '../../util'
 import { findPlaceByCandidatId } from '../../models/place'
+import PlaceModel from '../../models/place/place.model'
+
 import {
   setNowBefore12h,
   setNowAtNow,
@@ -167,15 +169,9 @@ describe('Get places available and display at 12h.', () => {
       ${'5'}       | ${candidatStatuses} | ${isFirstStatus ? 3 : 1} | ${isFirstStatus ? 3 : 1} | ${3}               | ${isFirstStatus ? 1 : 0} | ${isFirstStatus ? 1 : 0} | ${1}               | ${isFirstStatus ? '' : 'not'} | ${isFirstStatus ? '' : 'not'} | ${''}
       ${'6'}       | ${candidatStatuses} | ${isFirstStatus ? 3 : 1} | ${isFirstStatus ? 3 : 1} | ${3}               | ${isFirstStatus ? 1 : 0} | ${isFirstStatus ? 1 : 0} | ${1}               | ${isFirstStatus ? '' : 'not'} | ${isFirstStatus ? '' : 'not'} | ${''}
       `('for status:$status', ({ status, statuses, after12hExpected, after12h20Expected, after12h50Expected, selectedAfter12h, selectedAfter12h20, selectedAfter12h50, canBookedAfter12h, canBookedAfter12h20, canBookedAfter12h50 }) => {
-      beforeAll(async () => {
+      beforeAll((done) => {
         const moduleCandidatStatuses = require('../common/candidat-status-const')
         moduleCandidatStatuses.candidatStatuses = statuses
-
-        await updateCandidatById(idCandidat, {
-          status,
-          canBookFrom: getFrenchLuxon().plus({ days: hasPenalty ? 1 : -1 }),
-        },
-        )
 
         verifyAccesPlacesByCandidat.mockImplementation(async (req, res, next) => {
           const verifyCandidat = jest.requireActual('./middlewares/verify-candidat')
@@ -186,24 +182,48 @@ describe('Get places available and display at 12h.', () => {
           await verifyCandidat.verifyAccesPlacesByCandidat(req, res, nextTmp)
           next()
         })
+
+        updateCandidatById(idCandidat, {
+          status,
+          canBookFrom: getFrenchLuxon().plus({ days: hasPenalty ? 1 : -1 }),
+        },
+        ).catch((error) => {
+          throw error
+        }).finally(() => {
+          done()
+        })
+      })
+      afterEach((done) => {
+        PlaceModel.updateMany({ candidat: { $exists: true } }, { $set: { candidat: undefined, booked: undefined } }).exec().finally(() => {
+          done()
+        })
       })
 
       afterAll(() => {
         setNowAtNow()
       })
 
-      it('Should get 1 place for 75 when now is before 12h', async () => {
+      it('Should get 1 place for 75 when now is before 12h', (done) => {
         setNowBefore12h()
-        await expectedPlaces(1)
+        expectedPlaces(1).catch((error) => {
+          throw error
+        }).finally(() => {
+          done()
+        })
       })
 
-      it.each([
-        [after12hExpected, 0],
-        [after12h20Expected, 20],
-        [after12h50Expected, 50],
-      ])(`Should get %i places for 75 when now is after 12h%i for status ${status}`, async (npPlacesExpected, minutes) => {
+      it.each`
+        npPlacesExpected      | minutes
+        ${after12hExpected}   | ${0}
+        ${after12h20Expected} | ${20}
+        ${after12h50Expected} | ${50}
+      `(`Should get $npPlacesExpected places for 75 when now is after 12h$minutes for status ${status}`, ({ npPlacesExpected, minutes }, done) => {
         setNowAfter12h(minutes)
-        await expectedPlaces(npPlacesExpected)
+        expectedPlaces(npPlacesExpected).catch((error) => {
+          throw error
+        }).finally(() => {
+          done()
+        })
       })
 
       it.each`
@@ -213,17 +233,29 @@ describe('Get places available and display at 12h.', () => {
         ${selectedAfter12h50}   | ${50}
       `(`Should 200 with $npPlacesExpected available place before 12h when it is after 12h$minutes by center name and geo-departement for status ${status}`, async ({ npPlacesExpected, minutes }, done) => {
         setNowAfter12h(minutes)
-        expectedPlaceByNameCentreAndGeoDep(placesCreatedBefore.date, npPlacesExpected).finally(() => done())
+        expectedPlaceByNameCentreAndGeoDep(placesCreatedBefore.date, npPlacesExpected).catch((error) => {
+          throw error
+        }).finally(() => {
+          done()
+        })
       })
 
       it('Should 200 with no available place before 12h when it is after 12h by center name and geo-departement', (done) => {
         setNowBefore12h()
-        expectedPlaceByNameCentreAndGeoDep(placesCreatedBefore.date, 0).finally(() => done())
+        expectedPlaceByNameCentreAndGeoDep(placesCreatedBefore.date, 0).catch((error) => {
+          throw error
+        }).finally(() => {
+          done()
+        })
       })
 
       it('should not booked place by candidat with info bookedAt when it is before 12h', (done) => {
         setNowBefore12h()
-        expectedBookedFailed(placesCreatedBefore, idCandidat).finally(() => done())
+        expectedBookedFailed(placesCreatedBefore, idCandidat).catch((error) => {
+          throw error
+        }).finally(() => {
+          done()
+        })
       })
 
       it.each`
@@ -234,11 +266,15 @@ describe('Get places available and display at 12h.', () => {
       `('should $canBooked booked place by candidat with info bookedAt when it is after 12h$minutes', ({ canBooked, minutes }, done) => {
         setNowAfter12h(minutes)
         if (canBooked === 'not') {
-          expectedBookedFailed(placesCreatedBefore, idCandidat).finally(() => {
+          expectedBookedFailed(placesCreatedBefore, idCandidat).catch((error) => {
+            throw error
+          }).finally(() => {
             done()
           })
         } else {
-          expectedBooked(placesCreatedBefore, idCandidat).finally(() => {
+          expectedBooked(placesCreatedBefore, idCandidat).catch((error) => {
+            throw error
+          }).finally(() => {
             done()
           })
         }
