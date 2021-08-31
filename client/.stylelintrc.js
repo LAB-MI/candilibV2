dbf4d7f87bb6689d3c60b9cdaf9c56def169d0df
baudointran <baudointran@users.noreply.github.com>
2021-08-31 11:28:07 +0200

    update configuration for stylus

--- client/.stylelintrc.js
+++ client/.stylelintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
-  extends: 'stylelint-config-standard',
+  extends: ['stylelint-config-standard', 'stylelint-plugin-stylus/standard'],
+  rules: {
+    'stylus/block-opening-brace-space-before': 'always',
+    'stylus/pythonic': 'never',
+    'stylus/declaration-colon': 'always',
+    'stylus/semicolon': 'always',
+    'stylus/selector-list-comma': 'always',
+    'stylus/media-feature-colon': 'always',
+    'stylus/single-line-comment': 'never',
+    'declaration-colon-newline-after': 'always-multi-line',
+  },
 }
