529a12be43e543885ef29748e883f1544417329b
baudointran <baudointran@users.noreply.github.com>
2021-09-23 14:55:11 +0200

    fix-check-client-id

--- server/src/routes/middlewares/verify-user.js
+++ server/src/routes/middlewares/verify-user.js
@@ -1,3 +1,4 @@
+import { appLogger } from '../../util/logger'
 import { validate as uuidValidate, version as uuidVersion } from 'uuid'
 import { version } from '../../../package.json'
 
@@ -6,22 +7,34 @@ export const XUserId = 'X-USER-ID'
 export async function verifyUser (req, res, next) {
   const xUserId = req.get(XUserId)
   const { userId } = req
-  const clientId = req.headers['x-client-id'].split(`.${version}`)[0]
   const message = 'Votre lien de connexion n\'est pas valide, veuillez en demander un autre via le bouton "Déjà inscrit"'
+  const loggerInfo = {
+    request_id: req.request_id,
+    section: 'verify-user',
+    userId,
+  }
 
   try {
+    const clientId = req.headers['x-client-id'].split(`.${version}`)[0]
     const isVersion = await uuidVersion(clientId)
     const valideClientId = await uuidValidate(clientId)
 
     if (!xUserId || !userId || xUserId !== userId || !valideClientId || isVersion !== 4) {
-      throw Error(message)
+      const error = Error(message)
+      error.status = 401
+      throw error
     }
 
     next()
   } catch (error) {
-    return res.status(401).send({
+    appLogger.error({
+      ...loggerInfo,
+      description: error.messge,
+      error,
+    })
+    return res.status(error.status || 500).send({
       isTokenValid: false,
-      message: error.message === 'Invalid UUID' ? message : error.message,
+      message: message,
       success: false,
     })
   }
