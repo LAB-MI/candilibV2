d018d01bda7b9ddadb20f14d0bee669e8af0300c
jclaudan <jclaudan@users.noreply.github.com>
2021-10-19 17:30:15 +0200

    :construction_worker: Update pm2 config ecosystem

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,38 +1,39 @@
 module.exports = {
-  apps: [{
-    name: 'API',
-    script: 'dist/index.js',
-    instances: -2,
-    exec_mode: 'cluster',
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    autorestart: true,
-    log_file: '/dev/null',
-    out_file: '/dev/null',
-    error_file: '/dev/null',
-    merge_logs: true,
-    env: {
-      NODE_ENV: 'development',
+  apps: [
+    {
+      name: 'API',
+      script: 'dist/index.js',
+      instances: -1,
+      exec_mode: 'cluster',
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      autorestart: true,
+      log_file: '/dev/null',
+      out_file: '/dev/null',
+      error_file: '/dev/null',
+      merge_logs: true,
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
     },
-    env_production: {
-      NODE_ENV: 'production',
+    {
+      name: 'SDL',
+      script: 'dist/schedulers/index.js',
+      instances: 1,
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      autorestart: true,
+      log_file: '/dev/null',
+      out_file: '/dev/null',
+      error_file: '/dev/null',
+      merge_logs: true,
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
     },
-  },
-  {
-    name: 'SDL',
-    script: 'dist/schedulers/index.js',
-    instances: 1,
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    autorestart: true,
-    log_file: '/dev/null',
-    out_file: '/dev/null',
-    error_file: '/dev/null',
-    merge_logs: true,
-    env: {
-      NODE_ENV: 'development',
-    },
-    env_production: {
-      NODE_ENV: 'production',
-    },
-  }
-],
+  ],
 }
