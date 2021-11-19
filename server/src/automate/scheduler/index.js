457949843dfee3375bf3f2b018e66cbb74e97d5a
baudointran <baudointran@users.noreply.github.com>
2021-11-19 09:31:55 +0100

    get jobs automate

--- server/src/automate/scheduler/index.js
+++ server/src/automate/scheduler/index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import { start, status, stop } from './scheduler-controllers'
+import { getJobs } from './scheduler-job-controllers'
 
 const router = express.Router()
 
 router.post('/scheduler/start', start)
 router.post('/scheduler/stop', stop)
 router.get('/scheduler/status', status)
+
+router.get('/jobs/:name?', getJobs)
 
 export default router
