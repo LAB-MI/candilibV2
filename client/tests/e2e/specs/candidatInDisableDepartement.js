71c2ecaa2e4f149e2afa5ed5abad7b25afac406b
baudointran <baudointran@users.noreply.github.com>
2021-12-15 17:00:26 +0100

    test e2e for remove departements disabled

--- client/tests/e2e/specs/candidatInDisableDepartement.js
+++ client/tests/e2e/specs/candidatInDisableDepartement.js
@@ -19,4 +19,11 @@ describe('Connected candidat in a departement with disableAt', () => {
 
     cy.get('.t-info-disable-departement').contains('En raison du déploiement de RdvPermis dans votre département (94), l’application Candilib ne proposera plus de places d’examens après la date du 29/01/2022')
   })
+
+  it('Should have departement 25 and not departement 01 in choix de departement', () => {
+    cy.connectByMagicLink(magicLink)
+    cy.get('h2').should('contain', 'Choix du département')
+    cy.get('[role="list"]').should('not.contain', '01')
+    cy.get('[role="list"]').should('contain', '25')
+  })
 })
