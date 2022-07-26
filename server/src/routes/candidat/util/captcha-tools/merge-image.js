c373858a5734d2e5138688f51af597eb362cb34a
baudointran <baudointran@users.noreply.github.com>
2022-07-26 16:14:42 +0200

    create add text on image

--- server/src/routes/candidat/util/captcha-tools/merge-image.js
+++ server/src/routes/candidat/util/captcha-tools/merge-image.js
@@ -1,31 +1,16 @@
 
-import jimp, { MIME_PNG } from 'jimp'
-
-import mergeImg from './merge-image-tools/index.js'
 import { numberOfImages } from '../../../../config.js'
 import {
   drawTextInImage,
-  // modifyImage
 } from './manage-image-jimp.js'
 import joinImages from 'join-images'
 
 export const concatImages = async (images) => {
-  // const jimpFont = await jimp.loadFont(jimp.FONT_SANS_12_BLACK)
   const newImages = await Promise.all(images.map(async (image, index) => {
     try {
-      // TODO A finir
-      return image
-      // return drawTextInImage(image, `${index + 1}`)
-      //     const jimpImg = await modifyImage(image)
-
-      // const jimpCreated = await jimp.create(15, 15)
-      // jimpCreated.print(jimpFont, 0, 0, `${index + 1}`)
-
-      // const bufferImage = await jimpImg.getBufferAsync(jimp.MIME_PNG)
-      // const bufferImageIndex = await jimpCreated.getBufferAsync(jimp.MIME_PNG)
-
-      // const imageWithNumber = await mergeImg([bufferImage, bufferImageIndex], { direction: true })
-      //     return imageWithNumber
+      const imgIndex = await drawTextInImage(image, `${index + 1}`)
+      const newImage = await (await joinImages([image, imgIndex], { direction: 'vertical', offset: 10, color: { b: 255, g: 255, r: 255 } }))
+      return newImage.png().toBuffer()
     } catch (error) {
       return image
     }
@@ -35,7 +20,6 @@ export const concatImages = async (images) => {
   const img1 = await (await joinImages(newImages.slice(3, numberOfImages), { direction: 'horizontal', offset: 10, color: { b: 255, g: 255, r: 255 } })).png().toBuffer()
 
   const img = await joinImages([img0, img1], { direction: 'vertical', offset: 10, color: { b: 255, g: 255, r: 255 } })
-
   return img.png()
     .toBuffer()
 }
@@ -70,11 +54,6 @@ export async function streamImages (isRetina) {
 }
 
 export const getImageNamePic = async (frontendData) => {
-  // const font = await jimp.loadFont(`${__dirname}/fonts/poppins-bold/poppins-bold.fnt`)
-  // const sizeText = jimp.measureText(font, frontendData.imageName)
-  // const imagename = await jimp.create(sizeText, 22)
-  // imagename.print(font, 0, 0, frontendData.imageName)
-  // return imagename.getBase64Async(jimp.MIME_PNG)
   const bufferTextImg = await drawTextInImage(undefined, frontendData.imageName)
   return `data:image/png;base64,${bufferTextImg.toString('base64')}`
 }
